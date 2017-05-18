d4fae753ddd37389e35e666f8d7beed1ad34f570
George Stagg <george.stagg@ncl.ac.uk>
2017-05-18 15:46:54 +0100

    auto build main tex from config.yml

--- makeCourse/latex.py
+++ makeCourse/latex.py
@@ -1,13 +1,18 @@
 import os
+from datetime import datetime
 import glob
 import sys
+import pkg_resources
+import shutil
 from subprocess import Popen, PIPE 
+from jinja2 import Template
 
 def runPlastex(args,course_config,themePath,themeName,texFile):
-	cmd = 'HTML5TEMPLATES="%s" plastex --dir=%s/build/ --renderer=HTML5ncl --dollars --theme %s %s/%s' \
-				%(themePath,args.dir,themeName,args.dir,texFile)
+	buildDir = os.path.join(args.dir,'build')
+	cmd = 'HTML5TEMPLATES="%s" plastex --dir=%s --renderer=HTML5ncl --dollars --theme %s %s' \
+				%(themePath,buildDir,themeName,texFile)
 	if args.verbose:
-		print 'running plastex: %s'%cmd
+		print 'Running plastex: %s'%cmd
 	proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 	if args.veryverbose:
 		for line in iter(proc.stderr.readline, ''):
@@ -16,11 +21,35 @@ def runPlastex(args,course_config,themePath,themeName,texFile):
 	rc = proc.wait()
 	if rc != 0:
 		sys.stderr.write("Error: Something went wrong with the latex compilation! Quitting...\n")
-		sys.stderr.write("(Use -v for more information)\n")
+		sys.stderr.write("(Use -vv for more information)\n")
 		sys.exit(2)
 	elif args.verbose:
 		print 'Done!'
 	
+def copyWebsetupSty(args):
+	resource_package = "makeCourse"
+	resource_path = '/'.join(('misc', 'websetup.sty'))
+	webStyPath = pkg_resources.resource_filename(resource_package, resource_path)
+	target = os.path.join(args.dir,'websetup.sty')
+
+	if args.verbose:
+		print "    ", webStyPath, "=>", target
+	shutil.copy (webStyPath, target)
+	return target
+
+def buildMainTex(args,course_config):
+	mainTexPath = os.path.join(args.dir,'main-%s.tex'%os.urandom(2).encode('hex'))
+	if args.verbose:
+		print 'Building .tex file: %s'%	mainTexPath
+	resource_package = "makeCourse"
+	resource_path = '/'.join(('misc', 'main.jinja2'))
+	mainTexTemplate = pkg_resources.resource_string(resource_package, resource_path)
+	t = Template(mainTexTemplate)
+	mainTexRendered = t.render(course = course_config)
+	f = open(mainTexPath,"w") 
+	f.write(mainTexRendered) 
+	f.close() 
+	return mainTexPath
 
 def doLatexProcess(args,course_config):
 	if 'themePath' in course_config.keys():
@@ -35,19 +64,29 @@ def doLatexProcess(args,course_config):
 	else:
 		themeName = 'default'
 
-	if 'head_tex' in course_config.keys():
-		texFile = course_config['head_tex']
-	else:
-		texFile = args.dir+'.tex'
+	if args.verbose:
+		print 'Copying web style file websetup.sty...'	
+	newWebStyPath = copyWebsetupSty(args)
 
-	runPlastex(args,course_config,themePath,themeName,texFile)
+	if args.verbose:
+		print 'Building complete .tex...'
+	mainTexPath = buildMainTex(args, course_config)
+	
+	runPlastex(args,course_config,themePath,themeName,mainTexPath)
 
 	if args.verbose:
 		print 'Cleaning Up...'
 	for paux_file in glob.glob("*.paux"):
 		if args.verbose:
-			print '    %s'%paux_file
-		os.remove('%s'%paux_file)
+			print '    Deleted: %s'%paux_file
+		os.remove(paux_file)
+	if args.verbose:
+		print '    Deleted: %s'%newWebStyPath
+	os.remove(newWebStyPath)
+	if args.verbose:
+		print '    Deleted: %s'%mainTexPath
+	os.remove(mainTexPath)
+
 	if args.verbose:
 		print 'Done!'
 
