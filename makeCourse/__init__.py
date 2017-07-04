ce48013df9d77a72ec49145fcbfa5bcced647579
George Stagg <george.stagg@ncl.ac.uk>
2017-07-04 12:06:10 +0100

    update server

--- makeCourse/__init__.py
+++ makeCourse/__init__.py
@@ -1,6 +1,6 @@
 import re
 
-HACKMD_URL = "http://0.0.0.0:8080"
+HACKMD_URL = "http://makara.ncl.ac.uk:8080"
 
 def slugify(value):
 	return "".join([c for c in re.sub(r'\s+','_',value) if c.isalpha() or c.isdigit() or c=='_']).rstrip().lower()
