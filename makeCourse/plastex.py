25f573756700becc4260513e2881f959dff2b3b7
George Stagg <george.stagg@ncl.ac.uk>
2017-07-26 17:20:08 +0100

    start plastex update and removed separate chapter sections from config

--- makeCourse/plastex.py
+++ makeCourse/plastex.py
@@ -7,14 +7,16 @@ import shutil
 from subprocess import Popen, PIPE 
 from jinja2 import Template
 
-def runPlastex(args,course_config,themePath,themeName,texFile):
-	buildDir = os.path.join(args.dir,'build')
-	cmd = 'HTML5TEMPLATES="%s" plastex --dir=%s --renderer=HTML5ncl --dollars --theme %s %s' \
-				%(themePath,buildDir,themeName,texFile)
-	if args.verbose:
+def runPlastex(course_config,inFile,tmpDir):
+	outPath = os.path.join(course_config['args'].dir,tmpDir)
+	inPath = os.path.join(course_config['args'].dir,inFile)
+
+	cmd = 'plastex --dir=%s --split-level=-1 --renderer=HTML5ncl --dollars %s'%(outPath,inPath)
+
+	if course_config['args'].verbose:
 		print 'Running plastex: %s'%cmd
 	proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-	if args.veryverbose:
+	if course_config['args'].veryverbose:
 		for line in iter(proc.stderr.readline, ''):
 			print line
 		proc.stdout.close()
@@ -23,125 +25,5 @@ def runPlastex(args,course_config,themePath,themeName,texFile):
 		sys.stderr.write("Error: Something went wrong with the latex compilation! Quitting...\n")
 		sys.stderr.write("(Use -vv for more information)\n")
 		sys.exit(2)
-	elif args.verbose:
+	elif course_config['args'].verbose:
 		print 'Done!'
-	
-def runPdflatex(args,obj,sourcePath,pdfPath):
-	sourcePathDir = os.path.dirname(sourcePath)
-	pdfPathDir = os.path.abspath(os.path.dirname(pdfPath))
-	cmd = 'cd %s && pdflatex %s && mkdir -p %s && cp main.pdf %s'\
-			%(sourcePathDir,'main.tex',pdfPathDir,pdfPathDir)
-	if args.veryverbose:
-		print 'Running pdflatex: %s'%cmd
-	proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-	if args.veryverbose:
-		for line in iter(proc.stdout.readline, ''):
-			print line
-		proc.stdout.close()
-	rc = proc.wait()
-	if rc != 0:
-		sys.stderr.write("Error: Something went wrong with the latex compilation! Quitting...\n")
-		sys.stderr.write("(Use -vv for more information)\n")
-		sys.exit(2)
-	elif args.veryverbose:
-		print 'Done!'
-
-def copyWebsetupSty(args):
-	resource_package = "makeCourse"
-	resource_path = '/'.join(('misc', 'websetup.sty'))
-	webStyPath = pkg_resources.resource_filename(resource_package, resource_path)
-	target = os.path.join(args.dir,'websetup.sty')
-
-	if args.verbose:
-		print "    ", webStyPath, "=>", target
-	shutil.copy (webStyPath, target)
-	return target
-
-def buildMainTex(args,course_config):
-	mainTexPath = os.path.join(args.dir,'main-%s.tex'%os.urandom(2).encode('hex'))
-	if args.verbose:
-		print 'Building .tex file: %s'%	mainTexPath
-	resource_package = "makeCourse"
-	resource_path = '/'.join(('misc', 'main.jinja2'))
-	mainTexTemplate = pkg_resources.resource_string(resource_package, resource_path)
-	t = Template(mainTexTemplate)
-	mainTexRendered = t.render(course = course_config)
-	f = open(mainTexPath,"w") 
-	f.write(mainTexRendered) 
-	f.close() 
-	return mainTexPath
-
-def doLatexProcess(args,course_config):
-	if 'themePath' in course_config.keys():
-		themePath = course_config['themePath']
-	elif os.path.isabs(args.dir):
-		themePath = args.dir
-	else:
-		themePath = os.path.join(os.getcwd(),args.dir)
-
-	if 'theme' in course_config.keys():
-		themeName = course_config['theme']
-	else:
-		themeName = 'default'
-
-	if args.verbose:
-		print 'Copying web style file websetup.sty...'	
-	newWebStyPath = copyWebsetupSty(args)
-
-	if args.verbose:
-		print 'Building complete .tex...'
-	mainTexPath = buildMainTex(args, course_config)
-	
-	runPlastex(args,course_config,themePath,themeName,mainTexPath)
-
-	if args.verbose:
-		print 'Cleaning up after plastex...'
-	for paux_file in glob.glob("*.paux"):
-		if args.verbose:
-			print '    Deleted: %s'%paux_file
-		os.remove(paux_file)
-	if args.verbose:
-		print '    Deleted: %s'%newWebStyPath
-	os.remove(newWebStyPath)
-	if args.verbose:
-		print '    Deleted: %s'%mainTexPath
-	os.remove(mainTexPath)
-
-	if args.verbose:
-		print 'Done!'
-
-def doLatexPDFProcess(args,course_config):
-	if 'static_dir' in course_config.keys():
-		staticPath = course_config['static_dir']
-	else:
-		staticPath = 'static'
-
-	if args.verbose:
-		print 'Building .pdf files...'
-
-	for obj in course_config['structure']:
-		if obj['type'] == 'include' and 'content.tex' in obj['source']:
-			sourcePath = os.path.join(args.dir,obj['source'])
-			pdfPath = os.path.join(args.dir,staticPath,obj['source'].replace("content.tex", "main.pdf"))
-			if args.verbose:
-				print '    %s => %s'%(sourcePath, pdfPath)
-			runPdflatex(args,obj,sourcePath,pdfPath)	
-	
-	if args.verbose:
-		print 'Cleaning up after pdflatex...'
-	for obj in course_config['structure']:
-		if obj['type'] == 'include' and 'content.tex' in obj['source']:
-			compilePath = os.path.join(args.dir,obj['source'].replace("content.tex", "main"))
-			if args.verbose:
-				print '    Deleted: %s.log'%compilePath
-				print '    Deleted: %s.aux'%compilePath
-				print '    Deleted: %s.out'%compilePath
-				print '    Deleted: %s.pdf'%compilePath
-			os.remove('%s.log'%compilePath)
-			os.remove('%s.aux'%compilePath)
-			os.remove('%s.out'%compilePath)
-			os.remove('%s.pdf'%compilePath)
-	if args.verbose:
-		 print 'Done!'
-if __name__ == "__main__":
-    pass
--- makeCourse/process.py
+++ makeCourse/process.py
@@ -2,6 +2,7 @@ import sys
 import os
 import re
 import makeCourse.pandoc
+import makeCourse.plastex
 import makeCourse.latex
 import makeCourse.hackmd
 import makeCourse.slides
@@ -134,8 +135,12 @@ def buildIntroMDFile(course_config,obj):
 			sys.stderr.write("Error: Markdown file %s contains unsupported YAML header. Please remove the header, I'll make one automatically. Quitting...\n"%obj['source'])
 			sys.exit(2)
 	elif obj['source'][-4:] == '.tex':
-		#TODO: Do latex -> html snippet
-		newFileContent += '\n\n' + '# Unimplemented feature...'
+		#Do latex -> html snippet
+		tmpDir = '%s-plastex-index'%os.urandom(2).encode('hex')
+		makeCourse.plastex.runPlastex(course_config,obj['source'],tmpDir)
+		texContents = open(os.path.join(course_config['args'].dir,tmpDir,"index.html"), 'r').read()
+		texContents = burnInExtras(course_config,texContents)
+		newFileContent += '\n\n' + texContents
 	elif re.search(r'[^/\?:\s]+', obj['source']):
 		code = re.search(r'([^/\?:\s]+)', obj['source']).group(1)
 		mdContents = makeCourse.hackmd.getHackmdDocument(course_config,code)
@@ -159,89 +164,6 @@ def buildChapterMDFile(course_config,ch,part=False,pdf=False):
 	if 'content' in ch.keys() and 'source' in ch.keys():
 			sys.stderr.write("Error: Chapter %s contains both content and source elements; including both is invalid. Quitting...\n"%ch['title'])
 			sys.exit(2)
-
-	if 'content' in ch.keys():
-		if part:
-			newFile = '%s-%s_%s.md'%(os.urandom(2).encode('hex'),slugify(part['title']),slugify(ch['title']))
-		else:
-			newFile = '%s-%s.md'%(os.urandom(2).encode('hex'),slugify(ch['title']))
-		course_config['tempFiles'].append(newFile)
-		newFileContent = createYAMLheader(course_config,ch,part)
-
-		if course_config['args'].verbose:
-			print 'Building chapter file: %s'%newFile
-
-		for sec in ch['content']:
-			if isHidden(sec): continue
-			if sec['type'] == "section":
-				if sec['source'][-3:] == '.md':
-					if course_config['args'].verbose:
-						print '    Adding: %s'%sec['title']
-					mdContents = open(os.path.join(course_config['args'].dir,sec['source']), 'r').read()
-					if mdContents[:3] == '---':
-						if course_config['args'].verbose:
-							print '    Note: Markdown file %s contains a YAML header.'%sec['source']
-						mdContents = re.sub(r'^---.*?---\n','',mdContents)
-					print '    Burning in iframes & extras.'
-					mdContents = burnInExtras(course_config,mdContents,pdf)
-					newFileContent += '\n\n' + '# '+sec['title']+' {.tab-pane .fade}\n' + mdContents
-				elif sec['source'][-4:] == '.tex':
-					#TODO: Do latex -> html snippet
-					newFileContent += '\n\n' + '# '+sec['title']+' {.tab-pane .fade}\nUnimplemented feature...'
-				elif re.search(r'[^/\?:\s]+', sec['source']):
-					code = re.search(r'([^/\?:\s]+)', sec['source']).group(1)
-					mdContents = makeCourse.hackmd.getHackmdDocument(course_config,code)
-					mdContents = makeCourse.hackmd.getEmbeddedImages(course_config,mdContents)
-					print '    Burning in iframes & extras.'
-					mdContents = burnInExtras(course_config,mdContents,pdf)
-					newFileContent += '\n\n' + mdContents
-				else:
-					sys.stderr.write("Error: Unrecognised source type for %s, %s. Quitting...\n"%(ch['title'],sec['title']))
-					sys.exit(2)
-			elif sec['type'] == "numbas":
-				if course_config['args'].verbose:
-					print '    Adding section for numbas: %s'%sec['source']
-				newFileContent += '\n\n' + '# '+sec['title']+' {.tab-pane .fade}\n'+getNumbasHTML(sec['source'])+'"></iframe>'
-			elif sec['type'] == "beamer":
-				if sec['source'][-4:] == '.tex':
-					if course_config['args'].verbose:
-							print '    Adding section for beamer presentation: %s'%sec['title']
-					thePDF = makeCourse.latex.runPdflatex(course_config,sec['source'])
-					newFileContent += '\n\n' + '# '+sec['title']+' {.tab-pane .fade}\n<object class="loading" data="'+course_config['web_dir']+'static/'+thePDF+'" width="100%" height="500px" type="application/pdf"><embed src="'+course_config['web_dir']+'static/'+thePDF+'"></embed></object>\n<br><p class="pull-right"><a target="_blank" href="'+course_config['web_dir']+'static/'+thePDF+'">Click here to open slides in a new tab<a></p>'
-				else:
-					sys.stderr.write("Error: Unrecognised source type for %s, %s. Quitting...\n"%(ch['title'],sec['title']))
-					sys.exit(2)
-			elif sec['type'] == "slideshow":
-				if sec['source'][-3:] == '.md':
-					if course_config['args'].verbose:
-						print '    Adding: %s'%sec['title']
-					slideContents = open(os.path.join(course_config['args'].dir,sec['source']), 'r').read()
-					if slideContents[:3] == '---':
-						if course_config['args'].verbose:
-							print '    Note: Markdown file %s contains a YAML header.'%sec['source']
-						slideContents = re.sub(r'^---.*?---\n','',slideContents)
-					slidesHTMLFile = makeCourse.slides.runPandocForSlides(course_config,sec,slidesEmbedFile)
-					newFileContent += '\n\n' + '# '+sec['title']+' {.tab-pane .fade}\n<iframe src="'+slidesHTMLFile+'" width="100%" height="480px" style="overflow:hidden;" scrolling=no frameborder="0"></iframe>'
-				elif sec['source'][-4:] == '.tex':
-					#TODO: Do latex -> html snippet
-					sys.stderr.write("Error: Unimplemented feature... Quitting...\n")
-					sys.exit(2)
-				elif re.search(r'[^/\?:\s]+', sec['source']):
-					code = re.search(r'([^/\?:\s]+)', sec['source']).group(1)
-					newFileContent += '\n\n' + '# '+sec['title']+' {.tab-pane .fade}\n'+getSlidesHTML(course_config,code)
-				else:
-					sys.stderr.write("Error: Unrecognised source type for %s, %s. Quitting...\n"%(ch['title'],sec['title']))
-					sys.exit(2)
-			elif sec['type'] == "vimeo":
-				#TODO: include a section for a vimeo video
-				newFileContent += '\n\n' + '# '+sec['title']+' {.tab-pane .fade}\n'+getVimeoHTML(str(sec['source']))
-			else:
-				sys.stderr.write("Error: Unrecognised section type for %s, %s. Quitting...\n"%(ch['title'],sec['title']))
-				sys.exit(2)
-
-		f = open(os.path.join(course_config['args'].dir,newFile), 'w')
-		f.write(newFileContent)
-		f.close()
 
 	if 'source' in ch.keys():
 		if part:
