d93e4f97f1a69be225179f181da72cf9969de251
Christian Lawson-Perfect <christianperfect@gmail.com>
2017-09-06 13:41:06 +0100

    there's a much simpler way of getting rid of TeX aux files...

--- makeCourse/latex.py
+++ makeCourse/latex.py
@@ -2,6 +2,7 @@ import logging
 import os
 import pkg_resources
 import re
+import shutil
 import sys
 from subprocess import Popen, PIPE 
 
@@ -13,7 +14,8 @@ def runPdflatex(course_config,ch,inFile,inDir=None):
 	baseFile = re.sub(r'.tex$','',inFile)
 
 	cmd = ['pdflatex',inFile,'&&','pdflatex',inFile]
-	logger('Running pdflatex: {}'.format(os.path.join(inDir,inFile)))
+	logger.info('Running pdflatex: {}'.format(os.path.join(inDir,inFile)))
+
 	proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, cwd=inDir)
 	out, err = proc.communicate()
 	logger.debug(out)
@@ -25,56 +27,17 @@ def runPdflatex(course_config,ch,inFile,inDir=None):
 
 	outPath = os.path.join(course_config['build_dir'],ch['outFile']+".pdf")
 	logger.info('    Copying pdf output: %s => %s'%(os.path.join(inDir,baseFile+'.pdf'),outPath))
-	cmd = 'cp %s %s'%(os.path.join(inDir,baseFile+'.pdf'),outPath)
-	proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-	if course_config['args'].veryverbose:
-		logger.debug('    %s'%cmd )
-		logger.debug(proc.stdout.read())
-		proc.stdout.close()
-	rc = proc.wait()
-	if rc != 0:
-		sys.stderr.write("Error: Something went wrong copying resulting pdf! Quitting...\n")
-		sys.stderr.write("(Use -vv for more information)\n")
-		sys.exit(2)
+	shutil.copyfile(os.path.join(inDir,baseFile+'.pdf'), outPath)
 
-
-	logging.info('    Cleaning up after pdflatex...')
-	logging.info('        Deleting: %s.log'%os.path.join(course_config['args'].dir,baseFile))
-	logging.info('        Deleting: %s.aux'%os.path.join(course_config['args'].dir,baseFile))
-	logging.info('        Deleting: %s.out'%os.path.join(course_config['args'].dir,baseFile))
-	logging.info('        Deleting: %s.pdf'%os.path.join(course_config['args'].dir,baseFile))
-	logging.info('        Deleting: %s.snm'%os.path.join(course_config['args'].dir,baseFile))
-	logging.info('        Deleting: %s.nav'%os.path.join(course_config['args'].dir,baseFile))
-	logging.info('        Deleting: %s.toc'%os.path.join(course_config['args'].dir,baseFile))
-
-	try:
-		os.remove('%s.aux'%os.path.join(course_config['args'].dir,baseFile))
-	except OSError:
-		pass
-	try:
-		os.remove('%s.log'%os.path.join(course_config['args'].dir,baseFile))
-	except OSError:
-		pass
-	try:
-		os.remove('%s.out'%os.path.join(course_config['args'].dir,baseFile))
-	except OSError:
-		pass
-	try:
-		os.remove('%s.pdf'%os.path.join(course_config['args'].dir,baseFile))
-	except OSError:
-		pass
-	try:
-		os.remove('%s.snm'%os.path.join(course_config['args'].dir,baseFile))
-	except OSError:
-		pass
-	try:
-		os.remove('%s.nav'%os.path.join(course_config['args'].dir,baseFile))
-	except OSError:
-		pass
-	try:
-		os.remove('%s.toc'%os.path.join(course_config['args'].dir,baseFile))
-	except OSError:
-		pass
+	logger.info('    Cleaning up after pdflatex...')
+	extensions = ['.log','.aux','.out','.pdf','.snm','.nav','.toc']
+	for extension in extensions:
+		filename = '{base}{extension}'.format(base=os.path.join(inDir,baseFile), extension=extension)
+		logger.info('        Deleting: {}'.format(filename))
+		try:
+			os.remove(filename)
+		except OSError:
+			pass
 
 if __name__ == "__main__":
 	pass
