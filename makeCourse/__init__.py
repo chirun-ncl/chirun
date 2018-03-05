e0e5677f6991ae2042ba851db4e4c94779732ff9
George Stagg <george.stagg@newcastle.ac.uk>
2018-03-05 13:04:51 +0000

    Revert "Revert "Merge branch 'builder_class' into 'master'""
    This reverts commit a6a0f7d9dd9aec51d0117b44957b447566e8ba9d

--- makeCourse/__init__.py
+++ makeCourse/__init__.py
@@ -2,34 +2,17 @@ import re
 import sys
 import os
 import errno
+import yaml
 
 HACKMD_URL = "https://mas-coursebuild.ncl.ac.uk"
 
 def slugify(value):
 	return "".join([c for c in re.sub(r'\s+','_',value) if c.isalpha() or c.isdigit() or c=='_']).rstrip().lower()
 
-def temp_path(path):
-	tmp_dir = 'tmp'
-	if not os.path.exists(tmp_dir):
-		os.makedirs(tmp_dir)
-	return os.path.join(tmp_dir,'{}-{}'.format(os.urandom(2).encode('hex'),path))
-
 def isHidden(obj):
 	if 'hidden' in obj.keys():
 		if obj['hidden']:
 			return True
-	return False
-
-def containsMockTest(obj):
-	mocktest = filter(lambda item: item['type'] == 'mocktest', obj['structure'])
-	if mocktest:
-		return True
-	return False
-
-def getMockTest(obj):
-	mocktest = filter(lambda item: item['type'] == 'mocktest', obj['structure'])
-	if mocktest:
-		return mocktest[0]['source']
 	return False
 
 def gen_dict_extract(key, var):
@@ -49,7 +32,8 @@ def mkdir_p(path):
     try:
         os.makedirs(path)
     except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
+        if not os.path.isdir(path):
             raise
+
+def yaml_header(data):
+    return '---\n{}\n---\n\n'.format(yaml.dump(data,default_flow_style=False))
