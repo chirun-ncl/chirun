27d0ff0f58d06dd33d71ed23e2bd4e36ab7c7d25
Christian Lawson-Perfect <christianperfect@gmail.com>
2019-07-30 15:48:43 +0100

    move some of the processing stuff about
    makeCourse.process now just contains the Processor classes. the .process
    method is defined in makecourse itself, since it's quite short.
    
    In addition, the temporary files path is now relative to the working directory, not
    the source directory.

--- makeCourse/process.py
+++ makeCourse/process.py
@@ -1,6 +1,5 @@
 import logging
 from . import latex
-from .item import load_item
 from pathlib import Path
 import os
 import re
@@ -73,58 +72,17 @@ class PDFProcess(ItemProcess):
         super().visit(item)
 
     def visit_chapter(self, item):
-        self.course.makePDF(item)
+        self.makePDF(item)
 
     def visit_slides(self, item):
-        self.course.makePDF(item)
-
-
-class CourseProcessor:
-
-    def temp_path(self, subpath=None):
-        path = Path('tmp') / self.theme.path
-
-        if subpath:
-            path = path / subpath
-
-        mkdir_p(path)
-        return path
-
+        self.makePDF(item)
 
     def makePDF(self, item):
         ext = item.source.suffix
         if ext == '.tex':
-            latex.runPdflatex(self, item)
+            latex.runPdflatex(self.course, item)
         elif item.type == 'slides':
-            self.run_decktape(item)
+            self.course.run_decktape(item)
         else:
-            pandoc_item(self, item, template_file='notes.latex', out_format='pdf', force_local=True)
+            pandoc_item(self.course, item, template_file='notes.latex', out_format='pdf', force_local=True)
         item.has_pdf = True
-
-    processor_classes = [PDFProcess, RenderProcess]
-
-    def process(self):
-        logger.debug("Starting processing...")
-
-        logger.debug('Preprocessing structure...')
-        self.structure = [load_item(self, obj) for obj in self.config['structure']]
-
-        logger.debug('Deep exploring structure...')
-
-        self.partsEnabled = False
-        for item in self.structure:
-            if item.type=='part':
-                self.partsEnabled = True
-                break
-        if self.partsEnabled:
-            for item in self.structure:
-                if item.type not in ['introduction','part']:
-                    raise Exception("Error: Both parts and chapters found at top level. To fix: put all chapters inside parts or don't include parts at all. Quitting...\n")
-
-        processors = [p(self) for p in self.processor_classes]
-        for processor in processors:
-            logger.info(processor.name)
-            for item in self.structure:
-                processor.visit(item)
-
-        logger.debug('Done processing!')
