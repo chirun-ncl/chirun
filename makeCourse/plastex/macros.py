0580fc8ba55201f56967dd024c183133f974c542
Christian Lawson-Perfect <christianperfect@gmail.com>
2019-08-01 15:55:38 +0100

    add cssclass and div macros
    wrap their contents in a span or a div with the given classes

--- makeCourse/plastex/macros.py
+++ makeCourse/plastex/macros.py
@@ -1,4 +1,4 @@
-from plasTeX import Command, sourceChildren
+from plasTeX import Command, sourceChildren, Environment
 from plasTeX.Base.LaTeX import Math
 
 class numbas(Command):
@@ -17,3 +17,9 @@ class math(Math.math):
     @property
     def source(self):
         return '<script type="math/tex">{content}</script>'.format(content=sourceChildren(self).strip())
+
+class cssclass(Command):
+    args = '[ classes:str ] content:str'
+
+class div(Environment):
+    args = '[ classes:str ]'
