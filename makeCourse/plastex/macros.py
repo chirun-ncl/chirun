05eaf1b248979ff170376957efde922b46236ed8
George Stagg <george.stagg@newcastle.ac.uk>
2019-08-12 17:34:33 +0100

    Improve display of inline math

--- makeCourse/plastex/macros.py
+++ makeCourse/plastex/macros.py
@@ -1,5 +1,8 @@
 from plasTeX import Command, sourceChildren, Environment
 from plasTeX.Base.LaTeX import Math
+from plasTeX.Base.TeX import Primitives
+from plasTeX.Tokenizer import Token, EscapeSequence, Other
+
 
 class numbas(Command):
     args = '[ intro:str ] content:str'
@@ -13,10 +16,40 @@ class youtube(Command):
 class embed(Command):
     args = 'content:str'
 
-class math(Math.math):
+class cb_math(Math.math):
     @property
     def source(self):
-        return '<script type="math/tex">{content}</script>'.format(content=sourceChildren(self).strip())
+        return sourceChildren(self).strip()
+
+class displaymath(Math.displaymath):
+    @property
+    def source(self):
+        return sourceChildren(self).strip()
+
+class EqnarrayStar(Math.EqnarrayStar):
+    class ArrayCell(Math.EqnarrayStar.ArrayCell):
+        @property
+        def source(self):
+            return '\\displaystyle {content}'.format(content=sourceChildren(self, par=False).strip())
+
+class MathShift(Primitives.MathShift):
+    def invoke(self, tex):
+        inEnv = type(self).inEnv
+        current = self.ownerDocument.createElement('cb_math')
+        for t in tex.itertokens():
+            if t.catcode == Token.CC_MATHSHIFT:
+                current = self.ownerDocument.createElement('displaymath')
+            else:
+                tex.pushToken(t)
+            break
+        if inEnv and inEnv[-1] is not None and type(inEnv[-1]) is type(current):
+            inEnv.pop()
+            current.macroMode = Command.MODE_END
+            self.ownerDocument.context.pop(current)
+            return [current]
+        inEnv.append(current)
+        self.ownerDocument.context.push(current)
+        return [current]
 
 class cssclass(Command):
     args = '[ classes:str ] content:str'
