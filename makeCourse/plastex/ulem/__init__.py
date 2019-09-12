c36a25d24e9c460a6e7222e57382d243039d1ffd
George Stagg <george.stagg@newcastle.ac.uk>
2019-09-12 16:29:36 +0100

    Implement \uwave

--- makeCourse/plastex/ulem/__init__.py
+++ makeCourse/plastex/ulem/__init__.py
@@ -1,10 +1,17 @@
 from plasTeX.PackageResource import (
 	PackageResource, PackageCss, PackageJs, PackageTemplateDir)
-from plasTeX import Command, Environment
+from plasTeX import Command, Environment, sourceArguments
 
 def ProcessOptions(options, document):
     tpl = PackageTemplateDir(renderers='html5',package='ulem')
     document.addPackageResource([tpl])
 
 class uline(Command):
-    args = 's:str'
+    args = 'obj'
+
+class uwave(Command):
+    args = 'obj'
+    @property
+    def source(self):
+        return r'\stackrel{{{obj}}}{{\tiny\sim}}'.format(obj=sourceArguments(self).strip())
+
