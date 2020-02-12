d807b144f4e85ba2edbbc274dee9e304cc9c6524
George Stagg <george.stagg@newcastle.ac.uk>
2020-02-12 12:18:05 +0000

    Create plastex package for the "cancel" package

---
+++ makeCourse/plastex/cancel/__init__.py
@@ -0,0 +1,13 @@
+from plasTeX.PackageResource import (PackageResource, PackageCss, PackageJs, PackageTemplateDir)
+from plasTeX import Command, Environment, sourceArguments
+
+def ProcessOptions(options, document):
+    tpl = PackageTemplateDir(renderers='html5',package='cancel')
+    document.addPackageResource([tpl])
+
+class cancel(Command):
+    args = 'obj'
+    @property
+    def source(self):
+        return r'\require{{cancel}}\cancel{obj}'.format(obj=sourceArguments(self).strip())
+
