df4ddd0de6af86b5156f624521fefc7c37b26d14
Christian Lawson-Perfect <christianperfect@gmail.com>
2020-07-18 19:55:33 +0100

    when moving img into figure, split p tags if necessary
    a <figure> can not be inside a <p>. This changes the FigureExtension so
    that if a <figure> would end up under a <p> tag, it's moved after, and
    any siblings following the original <img> are moved to a new <p> tag
    after the <figure>.
    
    Fixes #10

--- makeCourse/markdownRenderer/markdown_figure/mdfigure.py
+++ makeCourse/markdownRenderer/markdown_figure/mdfigure.py
@@ -19,9 +19,12 @@ class FigureTreeprocessor(Treeprocessor):
         self._img_classes = img_classes
 
     def run(self, root):
+        moved_images = set()
         parent_map = {c: p for p in root.iter() for c in p}
         images = root.getiterator("img")
         for count, image in enumerate(images):
+            if image in moved_images:
+                continue
             alttext = image.attrib["alt"]
             image.set("alt", alttext)
             parent = parent_map[image]
@@ -39,10 +42,29 @@ class FigureTreeprocessor(Treeprocessor):
 
             figure.set("data-title", alttext)
             figure.tail = copy(image.tail)
-            parent.insert(idx, figure)
             parent.remove(image)
+            if parent.tag=='p':
+                grandparent = parent_map[parent]
+                pidx = list(grandparent).index(parent)
+                afters = list(parent)[idx+1:]
+                grandparent.insert(pidx+1,figure)
+                if len(afters) or figure.tail:
+                    afters_parent = etree.Element('p',parent.attrib)
+                    afters_parent.text = figure.tail
+                    figure.tail = None
+                    for a in afters:
+                        parent.remove(a)
+                        afters_parent.append(a)
+                        parent_map[a] = afters_parent
+                        parent_map[afters_parent] = grandparent
+                    grandparent.insert(pidx+2,afters_parent)
+                if not (len(list(parent)) or parent.text):
+                    grandparent.remove(parent)
+            else:
+                parent.insert(idx, figure)
             image.tail = markdown.util.AtomicString("")
             figure.append(image)
+            moved_images.add(image)
 
 class FigureExtension(Extension):
     """
