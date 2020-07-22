6d26087f30311c48bb52c57e75e6481d81aa7563
George Stagg <georgestagg@gmail.com>
2020-07-22 13:01:15 +0100

    Add custom argType for graphicx package
    A custom argType is now loaded when importing the graphicx package
    to help with parsing includegraphics arguments. The new custom argType
    is almost identical to a standard plastex dictionary argument type
    but explicitly checks for the keys "width" and "height" and parses
    them as dimensions. The other arguments are parsed in the standard way.
    
    This helps because a standard pastex dictionary can only have a single
    subtype, but the includegraphics arguments could be either numbers
    (scale), dimensions (width/height) or boolean (keepaspectratio). Using
    this custom dict argument type better handles widths and heights of
    the form 0.5\texwidth because they are then correctly calculated as
    dimensions.

--- makeCourse/plastex/graphicx/__init__.py
+++ makeCourse/plastex/graphicx/__init__.py
@@ -1,10 +1,90 @@
+import os,re
 import plasTeX
-from plasTeX import Command
+from plasTeX import Macro,Token,Command
 
 import plasTeX.Packages.graphicx
 
 class includegraphics(plasTeX.Packages.graphicx.includegraphics):
     altText = None
+    tex = None
+    args = '* [ options:gfxdict ] file:str'
+
+    def castGfxDictionary(self, tokens, type=dict, **kwargs):
+        delim = kwargs.get('delim')
+        if delim is None:
+            delim = ','
+        dictarg = type()
+        currentkey = []
+        currentvalue = None
+        while tokens:
+            current = tokens.pop(0)
+
+            if current.nodeType == Macro.ELEMENT_NODE:
+                currentvalue.append(current)
+                continue
+
+            # Found grouping
+            elif current.catcode == Token.CC_BGROUP:
+                level = 1
+                currentvalue.append(current)
+                while tokens:
+                    current = tokens.pop(0)
+                    if current.catcode == Token.CC_BGROUP:
+                        level += 1
+                    elif current.catcode == Token.CC_EGROUP:
+                        level -= 1
+                        if not level:
+                            break
+                    currentvalue.append(current)
+                currentvalue.append(current)
+                continue
+
+            # Found end-of-key delimiter
+            if current == '=':
+                currentvalue = []
+
+            # Found end-of-value delimiter
+            elif current == delim:
+                # Handle this later
+                pass
+
+            # Extend key
+            elif currentvalue is None:
+                currentkey.append(current)
+
+            # Extend value
+            else:
+                currentvalue.append(current)
+
+            # Found end-of-value delimiter
+            if current == delim or not tokens:
+                currentkey = self.tex.normalize(currentkey)
+                if currentkey == 'width' or currentkey == 'width':
+                    currentvalue = self.tex.cast(currentvalue, 'dimen')
+                else:
+                    currentvalue = self.tex.cast(currentvalue, 'str')
+                if currentvalue is None:
+                    currentvalue = True
+                dictarg[currentkey] = currentvalue
+                currentkey = []
+                currentvalue = None
+
+        if currentkey:
+            currentkey = self.tex.normalize(currentkey)
+            if currentkey == 'width' or currentkey == 'width':
+                currentvalue = self.tex.cast(currentvalue, 'dimen')
+            else:
+                currentvalue = self.tex.cast(currentvalue, 'str')
+            if currentvalue is None:
+                currentvalue = True
+            dictarg[currentkey] = currentvalue
+
+        return dictarg
+
+    def preParse(self, tex):
+        self.tex = tex
+        tex.argtypes.update({'gfxdict': self.castGfxDictionary})
+        return plasTeX.Packages.graphicx.includegraphics.preParse(self, tex)
 
     def invoke(self, tex):
         plasTeX.Packages.graphicx.includegraphics.invoke(self,tex)
