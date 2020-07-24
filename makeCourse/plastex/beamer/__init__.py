edcf1929f81eb8f35ff702cb9f5bd4ae01f5c04f
George Stagg <georgestagg@gmail.com>
2020-07-24 13:28:19 +0100

    Reorganise how beamer slides are generated
     * Handle a title slide.
     * Use blockType to suppress <p> tags.
     * Add a <div> around frames and move <hr> to after frames

--- makeCourse/plastex/beamer/__init__.py
+++ makeCourse/plastex/beamer/__init__.py
@@ -1,8 +1,25 @@
 from plasTeX.Packages.beamer import *
 import plasTeX.Packages.beamer
 
+class frame(plasTeX.Packages.beamer.frame):
+    blockType = True
+
+class frameenv(plasTeX.Packages.beamer.frameenv):
+    blockType = True
+
+class columns(plasTeX.Packages.beamer.columns):
+    blockType = True
+
 class column(plasTeX.Packages.beamer.column):
     args = '[ placement ] width:dimen'
+    blockType = True
 
 class columnenv(Environment):
     args = column.args
+    blockType = True
+
+class frametitle(plasTeX.Packages.beamer.frametitle):
+    blockType = True
+
+class framesubtitle(plasTeX.Packages.beamer.framesubtitle):
+    blockType = True
