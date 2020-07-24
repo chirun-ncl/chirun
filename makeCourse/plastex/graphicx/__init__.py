c2f1ad149e57ddc75ee37d6e258c216261366091
George Stagg <georgestagg@gmail.com>
2020-07-24 09:43:11 +0100

    Deprecate and remove \alt{} for alt text

--- makeCourse/plastex/graphicx/__init__.py
+++ makeCourse/plastex/graphicx/__init__.py
@@ -90,13 +90,10 @@ class includegraphics(plasTeX.Packages.graphicx.includegraphics):
         plasTeX.Packages.graphicx.includegraphics.invoke(self,tex)
         self.ownerDocument.userdata.setPath('packages/makecourse/currentimage', self)
 
-class alt(Command):
+class alttext(Command):
     args = 'text'
     def invoke(self, tex):
         Command.invoke(self,tex)
         doc = self.ownerDocument
         gfx = doc.userdata.getPath('packages/makecourse/currentimage')
         gfx.altText = self.attributes['text']
-
-class alttext(alt):
-    args = 'text'
