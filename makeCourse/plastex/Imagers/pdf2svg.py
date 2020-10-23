a483a075b9ce2cc4ab1102f9f7d00b86403035cd
George Stagg <georgestagg@gmail.com>
2020-10-23 11:54:06 +0100

    Namespace the imager tmp file and check exists

--- makeCourse/plastex/Imagers/pdf2svg.py
+++ makeCourse/plastex/Imagers/pdf2svg.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 from plasTeX.Imagers import VectorImager
+from pathlib import Path
 
 class PDFSVG(VectorImager):
     """ Imager that uses pdf2svg """
@@ -11,14 +12,19 @@ class PDFSVG(VectorImager):
     compiler = 'pdflatex'
 
     def executeConverter(self, output):
-        open('images-uncropped.pdf', 'wb').write(output.read())
-        subprocess.call(["pdfcrop", "images-uncropped.pdf", "images.pdf"], stdout=subprocess.PIPE)
+        if Path('mc-tmp-images-uc.pdf').exists():
+                raise Exception('Temporary pdf2svg imager file "mc-tmp-images-uc.pdf" already exists!')
+        if Path('mc-tmp-images.pdf').exists():
+                raise Exception('Temporary pdf2svg imager file "mc-tmp-images.pdf" already exists!')
+
+        open('mc-tmp-images-uc.pdf', 'wb').write(output.read())
+        subprocess.call(["pdfcrop", "mc-tmp-images-uc.pdf", "mc-tmp-images.pdf"], stdout=subprocess.PIPE)
 
         rc = 0
         page = 1
         while 1:
             filename = 'img%d.svg' % page
-            rc = subprocess.call(['pdf2svg', 'images.pdf', filename, str(page)], stdout=subprocess.PIPE)
+            rc = subprocess.call(['pdf2svg', 'mc-tmp-images.pdf', filename, str(page)], stdout=subprocess.PIPE)
             if rc:
                 break
 
