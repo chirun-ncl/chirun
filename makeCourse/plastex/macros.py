chirun-ncl/chirun

commit 245e684e8f76a96cd9a689f29468bb45a6c5d3bb

Tweak math rendering so that math in \text{} works
e.g. $\text{nested math: $x^2$}$ should now work.

makeCourse/plasTeXRenderer/Math.jinja2s
@@ -13,15 +13,17 @@ name: align eqnarray
 {% for row in obj %}
 {% if row.ref != None %}<div class="eqnarrayid" id="{{ row.id }}"></div>{% endif %}
 {% endfor %}
+<script type="math/tex; mode=display">
 \begin{align}
 {% for row in obj %}
 {{ row.source }}
 {% endfor %}
 \end{align}
+</script>
 </div>
 
 name: align* eqnarray*
-<span>{{ obj.source }}</span>
+<span><script type="math/tex; mode=display">{{ obj.source }}</script></span>
 
 name: equation
 <div class="eqn" id="{{ obj.id }}">

makeCourse/plastex/macros.py
@@ -48,3 +48,6 @@ class collapsehint(Command):
 class collapsesolution(Command):
     blockType = True
     args = 'hintText'
+
+class text(Primitives.BoxCommand):
+    args = 'self'

makeCourse/plastex/overrides.py
@@ -16,26 +16,13 @@ class label(Command):
         a = self.parse(tex)
         self.ownerDocument.context.catcode('_', catcode)
 
-class mbox(Primitives.BoxCommand):
-    args = 'self'
-    class math(Math.MathEnvironment):
-        @property
-        def source(self):
-            if self.hasChildNodes():
-                return u'$%s$' % sourceChildren(self)
-            return '$'
-
 class math(Math.math):
     @property
     def source(self):
-        return sourceChildren(self).strip()
+        if self.hasChildNodes():
+            return r'\({}\)'.format(sourceChildren(self))
+        return r'\('
 Math.math = math
-
-class displaymath(Math.displaymath):
-    @property
-    def source(self):
-        return sourceChildren(self).strip()
-Math.displaymath = displaymath
 
 class MathEnvironment(Math.Environment):
     mathMode = True

makeCourse/themes/accessible/plastex/Math.jinja2s
@@ -1,31 +1,0 @@
-name: cb_math math
-<script type="math/tex">{{ obj.source }}</script>
-
-name: ensuremath gather gather* flalign flalign* \
-      multline multline* alignat alignat* split DeclareMathOperator
-<span class="dmath"><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: displaymath equation*
-<span class="dmath"><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: align eqnarray
-<div id="{{ obj.id }}">
-{% for row in obj %}
-{% if row.ref != None %}<div class="eqnarrayid" id="{{ row.id }}"></div>{% endif %}
-{% endfor %}
-<script type="math/tex; mode=display">
-\begin{align}
-{% for row in obj %}
-{{ row.source }}
-{% endfor %}
-\end{align}
-</script>
-</div>
-
-name: align* eqnarray*
-<span><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: equation
-<div class="eqn" id="{{ obj.id }}">
-<script type="math/tex; mode=display">{{ obj.source }}</script>
-</div>

makeCourse/themes/default/plastex/Math.jinja2s
@@ -1,31 +1,0 @@
-name: cb_math math
-<script type="math/tex">{{ obj.source }}</script>
-
-name: ensuremath gather gather* flalign flalign* \
-      multline multline* alignat alignat* split DeclareMathOperator
-<span class="dmath"><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: displaymath equation*
-<span class="dmath"><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: align eqnarray
-<div id="{{ obj.id }}">
-{% for row in obj %}
-{% if row.ref != None %}<div class="eqnarrayid" id="{{ row.id }}"></div>{% endif %}
-{% endfor %}
-<script type="math/tex; mode=display">
-\begin{align}
-{% for row in obj %}
-{{ row.source }}
-{% endfor %}
-\end{align}
-</script>
-</div>
-
-name: align* eqnarray*
-<span><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: equation
-<div class="eqn" id="{{ obj.id }}">
-<script type="math/tex; mode=display">{{ obj.source }}</script>
-</div>

makeCourse/themes/standalone/plastex/Math.jinja2s
@@ -1,31 +1,0 @@
-name: cb_math math
-<script type="math/tex">{{ obj.source }}</script>
-
-name: ensuremath gather gather* flalign flalign* \
-      multline multline* alignat alignat* split DeclareMathOperator
-<span class="dmath"><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: displaymath equation*
-<span class="dmath"><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: align eqnarray
-<div id="{{ obj.id }}">
-{% for row in obj %}
-{% if row.ref != None %}<div class="eqnarrayid" id="{{ row.id }}"></div>{% endif %}
-{% endfor %}
-<script type="math/tex; mode=display">
-\begin{align}
-{% for row in obj %}
-{{ row.source }}
-{% endfor %}
-\end{align}
-</script>
-</div>
-
-name: align* eqnarray*
-<span><script type="math/tex; mode=display">{{ obj.source }}</script></span>
-
-name: equation
-<div class="eqn" id="{{ obj.id }}">
-<script type="math/tex; mode=display">{{ obj.source }}</script>
-</div>

commit 7403238f47aa557998c11ca88a7adf3c8fb70c86

Fix regression introduced in 245e684
The commit above re-introduced a bug when using less than symbols which
can be wrongfully interpreted as HTML tags. The fix is to wrap
mathematics in <script> tags -- but this must be done carefully with
regards to BoxCommands so that nested <script> tags are not generated.

makeCourse/plasTeXRenderer/Math.jinja2s
@@ -3,10 +3,10 @@ name: math
 
 name: ensuremath gather gather* flalign flalign* \
       multline multline* alignat alignat* split DeclareMathOperator
-<span class="dmath">{{ obj.source }}</span>
+<span class="dmath"><script type="math/tex; mode=display">{{ obj.source }}</script></span>
 
 name: displaymath equation*
-<span class="dmath">{{ obj.source }}</span>
+<span class="dmath"><script type="math/tex; mode=display">{{ obj.source }}</script></span>
 
 name: align eqnarray
 <div id="{{ obj.id }}">
@@ -27,5 +27,5 @@ name: align* eqnarray*
 
 name: equation
 <div class="eqn" id="{{ obj.id }}">
-{{ obj.source }}
+<script type="math/tex; mode=display">{{ obj.source }}</script>
 </div>

makeCourse/plastex/macros.py
@@ -49,9 +49,6 @@ class collapsesolution(Command):
     blockType = True
     args = 'hintText'
 
-class text(Primitives.BoxCommand):
-    args = 'self'
-
 class alttext(Command):
     args = 'text'
     def invoke(self, tex):

makeCourse/plastex/overrides.py
@@ -17,13 +17,37 @@ class label(Command):
         a = self.parse(tex)
         self.ownerDocument.context.catcode('_', catcode)
 
+# Overrive boxcommands inside MathJaX to avoid extra <script type="math/tex">
+class BoxCommand(Primitives.BoxCommand):
+    class math(Math.math):
+        @property
+        def source(self):
+            if self.hasChildNodes():
+                return u'\(%s\)' % sourceChildren(self)
+            return '\('
+class hbox(BoxCommand): pass
+class vbox(BoxCommand): pass
+class text(BoxCommand):
+    args = 'self'
+class mbox(BoxCommand):
+    args = 'self'
+class TextCommand(BoxCommand):
+    pass
+
+# Use <script type="math/tex"> to avoid problems with less than symbol in MathJax
 class math(Math.math):
     @property
     def source(self):
         if self.hasChildNodes():
-            return r'\({}\)'.format(sourceChildren(self))
-        return r'\('
+            return r'<script type="math/tex">{}</script>'.format(sourceChildren(self))
+        return ''
 Math.math = math
+
+class displaymath(Math.displaymath):
+     @property
+     def source(self):
+         return sourceChildren(self).strip()
+Math.displaymath = displaymath
 
 class MathEnvironment(Math.Environment):
     mathMode = True