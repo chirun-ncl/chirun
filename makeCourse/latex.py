chirun-ncl/chirun

commit 6567ee7e19071519d2971f451d6b46d25713a295

Detect if biblatex is required and run it if so
Also tweak the Bibliography templates to react to changes to the output
when using article/report documentclasses.

makeCourse/latex.py
@@ -7,29 +7,54 @@ from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
+class LatexRunner(object):
+    compiler = 'pdflatex'
+    args = None
+    wd = None
 
-def runPdflatex(course, item):
-    inDir = course.get_root_dir() / item.source.parent
-    stdout_tail = collections.deque(maxlen=8)
+    def __init__(self,filename, wd='.'):
+        self.wd = wd
+        self.args = [str(filename), '-halt-on-error']
 
-    cmd = ['pdflatex', '-halt-on-error', str(item.in_file)]
-    logger.info('Running pdflatex: {}'.format(inDir / item.in_file))
-
-    # Run pdflatex twice to resolve labels, even if num_pdf_runs = 1
-    for _ in range(2):
-        proc = Popen(cmd, stdout=PIPE, stderr=PIPE, cwd=str(inDir), universal_newlines=True)
+    def exec(self):
+        stdout_tail = collections.deque(maxlen=8)
+        cmd = [self.compiler] + self.args
+        logger.info('Running {}: {}'.format(self.compiler, self.wd / self.args[0]))
+        proc = Popen(cmd, stdout=PIPE, stderr=PIPE, cwd=str(self.wd), universal_newlines=True)
         for stdout_line in iter(proc.stdout.readline, ""):
             if not stdout_line.isspace():
                 stdout_tail.append(stdout_line)
             logger.debug(stdout_line)
-
         out, err = proc.communicate()
-
         if proc.returncode != 0:
             logger.error(err)
             raise Exception("Error: Something went wrong running: {}\n\n{}".format(' '.join(cmd), ''.join(stdout_tail)))
 
-    inPath = inDir / item.base_file.with_suffix('.pdf')
+class BibtexRunner(LatexRunner):
+    compiler = 'bibtex'
+    args = None
+
+    def __init__(self,aux_filename, wd='.'):
+        self.wd = wd
+        self.args = [str(aux_filename.with_suffix(''))]
+
+def runPdflatex(course, item):
+    in_dir = course.get_root_dir() / item.source.parent
+    tex_filename = in_dir / item.in_file
+    aux_filename = in_dir / item.in_file.with_suffix('.aux')
+
+    LatexRunner(tex_filename, in_dir).exec()
+
+    # Test for bibtex commands
+    with open(aux_filename) as f:
+        f_read = f.read()
+        if r'\bibstyle' in f_read or r'\bibdata' in f_read:
+            BibtexRunner(aux_filename, in_dir).exec()
+            LatexRunner(tex_filename, in_dir).exec()
+
+    LatexRunner(tex_filename, in_dir).exec()
+
+    inPath = in_dir / item.base_file.with_suffix('.pdf')
     outPath = course.get_build_dir() / item.named_out_file.with_suffix('.pdf')
     logger.debug('    Creating directory for pdf output: {outDir}'.format(outDir=outPath.parent))
     Path.mkdir(outPath.parent, parents=True, exist_ok=True)
@@ -40,7 +65,7 @@ def runPdflatex(course, item):
         logger.info('    Cleaning up after pdflatex...')
         extensions = ['.log', '.aux', '.out', '.pdf', '.snm', '.nav', '.toc']
         for extension in extensions:
-            filename = '{base}{extension}'.format(base=inDir / item.base_file, extension=extension)
+            filename = '{base}{extension}'.format(base=in_dir / item.base_file, extension=extension)
             logger.debug('        Deleting: {}'.format(filename))
             try:
                 os.remove(filename)

makeCourse/plasTeXRenderer/Bibliography.jinja2s
@@ -1,5 +1,9 @@
 name: thebibliography
-<h2>Bibliography</h2>
+{% if obj.ownerDocument.context.bibliography.level == 1 %}
+	<h2 id="{{obj.id}}">References</h2>
+{% else %}
+	<h1 id="{{obj.id}}">Bibliography</h1>
+{% endif %}
 <ul class="bibliography">
 {% for item in obj %}
 	<li style="list-style: none" id={{ item.id }}> <div class="bibcite">[{{ item.bibcite }}]</div> {{ item }} </li>
@@ -9,10 +13,9 @@ name: thebibliography
 name: bibliographystyle
 
 name: bibliography
-<div class="bibliography">
-<h1>{{ obj.title }}</h1>
+<section class="section bibliography">
 {{ obj }}
-</div>
+</section>
 
 name: cite
 <span class="cite">

commit 3e2ade9f5f992c85ff33269bbe02ad1acf3d5634

Fix path bug introduced in 6567ee7

makeCourse/latex.py
@@ -40,19 +40,18 @@ class BibtexRunner(LatexRunner):
 
 def runPdflatex(course, item):
     in_dir = course.get_root_dir() / item.source.parent
-    tex_filename = in_dir / item.in_file
     aux_filename = in_dir / item.in_file.with_suffix('.aux')
 
-    LatexRunner(tex_filename, in_dir).exec()
+    LatexRunner(item.in_file, in_dir).exec()
 
     # Test for bibtex commands
     with open(aux_filename) as f:
         f_read = f.read()
         if r'\bibstyle' in f_read or r'\bibdata' in f_read:
-            BibtexRunner(aux_filename, in_dir).exec()
-            LatexRunner(tex_filename, in_dir).exec()
+            BibtexRunner(item.in_file, in_dir).exec()
+            LatexRunner(item.in_file, in_dir).exec()
 
-    LatexRunner(tex_filename, in_dir).exec()
+    LatexRunner(item.in_file, in_dir).exec()
 
     inPath = in_dir / item.base_file.with_suffix('.pdf')
     outPath = course.get_build_dir() / item.named_out_file.with_suffix('.pdf')