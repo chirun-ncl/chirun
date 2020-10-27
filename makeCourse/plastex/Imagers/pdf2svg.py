da3b811bcc0c60dbd604506145001e3357739e7a
George Stagg <georgestagg@gmail.com>
2020-10-27 13:05:48 +0000

    Handle filename collisions in the pdf2svg Imager

--- makeCourse/plastex/Imagers/pdf2svg.py
+++ makeCourse/plastex/Imagers/pdf2svg.py
@@ -2,6 +2,8 @@
 
 import os
 import subprocess
+import os
+import binascii
 from plasTeX.Imagers import VectorImager
 from pathlib import Path
 
@@ -10,21 +12,23 @@ class PDFSVG(VectorImager):
     fileExtension = '.svg'
     verifications = ['pdflatex --help', 'which pdf2svg', 'pdfcrop --help']
     compiler = 'pdflatex'
+    imagesFilename = 'tmp-images-uc.pdf'
+    croppedImagesFilename = 'tmp-images.pdf' 
 
     def executeConverter(self, output):
-        if Path('mc-tmp-images-uc.pdf').exists():
-                raise Exception('Temporary pdf2svg imager file "mc-tmp-images-uc.pdf" already exists!')
-        if Path('mc-tmp-images.pdf').exists():
-                raise Exception('Temporary pdf2svg imager file "mc-tmp-images.pdf" already exists!')
+        while Path(self.imagesFilename).exists():
+            self.imagesFilename = binascii.b2a_hex(os.urandom(5)).decode("utf-8")+'-'+self.imagesFilename
+        while Path(self.croppedImagesFilename).exists():
+            self.croppedImagesFilename = binascii.b2a_hex(os.urandom(5)).decode("utf-8")+'-'+self.croppedImagesFilename
 
-        open('mc-tmp-images-uc.pdf', 'wb').write(output.read())
-        subprocess.call(["pdfcrop", "mc-tmp-images-uc.pdf", "mc-tmp-images.pdf"], stdout=subprocess.PIPE)
+        open(self.imagesFilename, 'wb').write(output.read())
+        subprocess.call(["pdfcrop", self.imagesFilename, self.croppedImagesFilename], stdout=subprocess.PIPE)
 
         rc = 0
         page = 1
         while 1:
             filename = 'img%d.svg' % page
-            rc = subprocess.call(['pdf2svg', 'mc-tmp-images.pdf', filename, str(page)], stdout=subprocess.PIPE)
+            rc = subprocess.call(['pdf2svg', self.croppedImagesFilename, filename, str(page)], stdout=subprocess.PIPE)
             if rc:
                 break
 
