7570838fd59a265812ca01ee2b132911c41d6b68
George Stagg <georgestagg@gmail.com>
2020-11-26 11:28:42 +0000

    Support scale option in graphicx for PDF files

--- makeCourse/plastex/graphicx/__init__.py
+++ makeCourse/plastex/graphicx/__init__.py
@@ -1,13 +1,117 @@
 import os,re
 import plasTeX
 from plasTeX import Macro,Token,Command
+from pathlib import Path
 from plasTeX.Packages.graphicx import *
 import plasTeX.Packages.graphicx
+
+class UnsupportedFiletypeException(Exception):
+    pass
 
 class includegraphics(plasTeX.Packages.graphicx.includegraphics):
     altText = None
     tex = None
     args = '* [ options:gfxdict ] file:str'
+
+    def getImageSize(self,img):
+        imgpath = Path(img)
+        imgext = imgpath.suffix
+        PILext = ['.png','.jpg','.jpeg','.gif','.eps','.ps']
+        PDFext = ['.pdf']
+
+        if imgext in PILext:
+            from PIL import Image
+            w, h = Image.open(img).size
+            return (w,h)
+        elif imgext in PDFext:
+            from PyPDF2 import PdfFileReader
+            pdfImage = PdfFileReader(open(img, 'rb'))
+            box = pdfImage.getPage(0).mediaBox
+            return (box.getWidth(), box.getHeight())
+        else:
+            raise UnsupportedFiletypeException("Reading the size of a {} file is not supported".format(imgext))
+
+        return None
+
+    def invoke(self, tex):
+        res = Command.invoke(self, tex)
+
+        f = self.attributes['file']
+        ext = self.ownerDocument.userdata.getPath(
+                      'packages/%s/extensions' % self.packageName,
+                      ['.png','.jpg','.jpeg','.gif','.pdf','.ps','.eps'])
+        paths = self.ownerDocument.userdata.getPath(
+                        'packages/%s/paths' % self.packageName, ['.'])
+        img = None
+
+        # Check for file using graphicspath
+        for p in paths:
+            for e in ['']+ext:
+                fname = os.path.join(p,f+e)
+                if os.path.isfile(fname):
+                    img = os.path.abspath(fname)
+                    break
+            if img is not None:
+                break
+
+        # Check for file using kpsewhich
+        if img is None:
+            for e in ['']+ext:
+                try:
+                    img = os.path.abspath(tex.kpsewhich(f+e))
+                    break
+                except (OSError, IOError):
+                    pass
+
+        options = self.attributes['options']
+
+        if options is not None:
+
+            scale = options.get('scale')
+            if scale is not None and img is not None:
+                scale = float(scale)
+                w, h = self.getImageSize(img)
+                self.style['width'] = '%spx' % (w * scale)
+                self.style['height'] = '%spx' % (h * scale)
+
+            height = options.get('height')
+            if height is not None:
+                self.style['height'] = height
+
+            width = options.get('width')
+            if width is not None:
+                self.style['width'] = width
+
+            def getdimension(s):
+                m = re.match(r'^([\d\.]+)\s*([a-z]*)$', s)
+                if m and '.' in m.group(1):
+                    return float(m.group(1)), m.group(2)
+                elif m:
+                    return int(m.group(1)), m.group(2)
+
+            keepaspectratio = options.get('keepaspectratio')
+            if img is not None and keepaspectratio == 'true' and \
+               height is not None and width is not None:
+                w, h = self.getImageSize(img)
+
+                height, hunit = getdimension(height)
+                width, wunit = getdimension(width)
+
+                scalex = float(width) / w
+                scaley = float(height) / h
+
+                if scaley > scalex:
+                    height = h * scalex
+                else:
+                    width = w * scaley
+
+                self.style['width'] = '%s%s' % (width, wunit)
+                self.style['height'] = '%s%s' % (height, hunit)
+
+        self.imageoverride = img
+        self.ownerDocument.userdata.setPath('packages/makecourse/currentimage', self)
+
+        return res
 
     def castGfxDictionary(self, tokens, type=dict, **kwargs):
         delim = kwargs.get('delim')
@@ -85,8 +189,3 @@ class includegraphics(plasTeX.Packages.graphicx.includegraphics):
         self.tex = tex
         tex.argtypes.update({'gfxdict': self.castGfxDictionary})
         return plasTeX.Packages.graphicx.includegraphics.preParse(self, tex)
-
-    def invoke(self, tex):
-        plasTeX.Packages.graphicx.includegraphics.invoke(self,tex)
-        self.ownerDocument.userdata.setPath('packages/makecourse/currentimage', self)
-
