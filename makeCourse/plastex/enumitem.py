9e9d241524cb677ca2a11046e60aaf17cc2efa19
George Stagg <georgestagg@gmail.com>
2021-01-27 11:50:15 +0000

    Partial attempt to implement enumitem
    First attempt at implementing the label and ref arguments for the enumitem package

--- makeCourse/plastex/enumitem.py
+++ makeCourse/plastex/enumitem.py
@@ -1,2 +1,102 @@
-from plasTeX import Command, Environment
+from plasTeX import Text, Command, Macro, Environment, encoding
+from plasTeX.Tokenizer import Token
+from plasTeX.DOM import Text
+from plasTeX.Base.LaTeX import Math
+from plasTeX.Base.LaTeX.Lists import List
+from makeCourse.plastex.overrides.lists import numToRoman
+import collections
+import re
 
+class enumerate_(List):
+    macroName = 'enumerate'
+    args = '[ options:dict:nox ]'
+    refLabel = None
+
+    class item(List.item):
+        def invoke(self,tex):
+            ret = List.item.invoke(self, tex)
+            self.refLabel = enumerate_.refLabel
+            return ret
+
+        @property
+        def ref(self):
+            if self.refLabel:
+                try:
+                    position = int(self.origref.textContent)
+                    alph = encoding.stringletters()[position - 1]
+                    t = re.sub(r'_Alph_',alph.upper(), str(self.refLabel))
+                    t = re.sub(r'_alph_',alph.lower(), t)
+                    t = re.sub(r'_roman_',numToRoman(position), t)
+                    t = re.sub(r'_Roman_',numToRoman(position).lower(), t)
+                    t = re.sub(r'_arabic_',str(position), t)
+                    return t
+                except:
+                    pass
+            return self.origref
+
+        @ref.setter
+        def ref(self, value):
+            self.origref = value
+
+    class arabicStar(Command):
+        macroName = 'arabic'
+        args = '*'
+        def invoke(self,tex):
+            a = self.parse(tex)
+            return tex.textTokens('_arabic_')
+
+    class romanStar(Command):
+        macroName = 'roman'
+        args = '*'
+        def invoke(self,tex):
+            a = self.parse(tex)
+            return tex.textTokens('_roman_')
+
+    class RomanStar(Command):
+        macroName = 'Roman'
+        args = '*'
+        def invoke(self,tex):
+            a = self.parse(tex)
+            return tex.textTokens('_Roman_')
+
+    class alphStar(Command):
+        macroName = 'alph'
+        args = '*'
+        def invoke(self,tex):
+            a = self.parse(tex)
+            return tex.textTokens('_alph_')
+
+    class AlphStar(Command):
+        macroName = 'Alph'
+        args = '*'
+        def invoke(self,tex):
+            a = self.parse(tex)
+            return tex.textTokens('_Alph_')
+
+    def invoke(self, tex):
+        List.invoke(self,tex)
+        enumerate_.refLabel = None
+        if 'options' in self.attributes:
+            opt = self.attributes['options']
+            if 'label' in opt:
+                self.listLabel = opt['label']
+            if 'ref' in opt:
+                enumerate_.refLabel = opt['ref']
+
+    def term(self, position):
+        alph = encoding.stringletters()[position - 1]
+        if self.listLabel:
+            t = re.sub(r'_Alph_',alph.upper(), str(self.listLabel))
+            t = re.sub(r'_alph_',alph.lower(), t)
+            t = re.sub(r'_roman_',numToRoman(position), t)
+            t = re.sub(r'_Roman_',numToRoman(position).lower(), t)
+            t = re.sub(r'_arabic_',str(position), t)
+        elif self.listDepth == 2:
+            t = '({})'.format(alph.lower())
+        elif self.listDepth == 3:
+            t = '{}.'.format(numToRoman(position).lower())
+        elif self.listDepth == 4:
+            t = '{}.'.format(alph.upper())
+        else:
+            t = '{}.'.format(position)
+        return t
