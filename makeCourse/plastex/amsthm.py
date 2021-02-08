7c1c0b65c9d96b9d6c7414908f28b34c64a7e861
George Stagg <georgestagg@gmail.com>
2021-02-08 12:16:36 +0000

    Tweak amsthm package styles slightly
    Adds some bold and italic styles to amsthm output. This is handled
    better in plasTeX 3 and so this override can be removed with others when
    it is released.

---
+++ makeCourse/plastex/amsthm.py
@@ -0,0 +1,59 @@
+from plasTeX import Environment, Command, dimen, TeXFragment
+
+class swapnumbers(Command):
+    pass
+
+class theoremstyle(Command):
+    args = 'style:str'
+
+class newtheoremstyle(Command):
+    args = 'name:str above:dimen below:dimen bodyfont indentamount:dimen headfont punctuation:str between:dimen headspec'
+
+class newtheorem(Command):
+    args = '* name:str [ shared:str ] header:str [ parent:str]'
+
+    def invoke(self, tex):
+        self.parse(tex)
+        a = self.attributes
+        name = str(a['name'])
+        header = a['header']
+        star = a['*modifier*'] == '*'
+        parent = a['parent']
+        shared = a['shared']
+
+        if star:
+            counter = None
+        else:
+            if parent and not shared:
+                self.ownerDocument.context.newcounter(name, initial=0, resetby=parent)
+                self.ownerDocument.context.newcommand("the"+name, 0,
+                                        "\\arabic{%s}.\\arabic{%s}"%(parent,name))
+                counter = name
+            elif shared:
+                counter = shared
+            else:
+                counter = name
+                self.ownerDocument.context.newcounter(name, initial=0)
+                self.ownerDocument.context.newcommand("the"+name, 0, "\\arabic{%s}" %name)
+
+        data = {'nodeName': 'thmenv',
+                'thmName': name,
+                'args': '[title]',
+                'counter': counter,
+                'caption': header,
+                'forcePars': True,
+            }
+        th = type(name, (Environment,), data)
+        self.ownerDocument.context.addGlobal(name, th)
+
+class qedhere(Command):
+    pass
+
+class proof(Environment):
+    blockType = True
+    args ='[caption]'
+    forcePars= True
+
+    def digest(self, tokens):
+        Environment.digest(self, tokens)
+        self.caption = self.attributes.get('caption', '')
