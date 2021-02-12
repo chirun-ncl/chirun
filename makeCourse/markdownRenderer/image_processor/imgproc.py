eed2d516c803fd0557edf01cb4b779b28b1726eb
George Stagg <georgestagg@gmail.com>
2021-02-12 15:38:00 +0000

    Fix PDF and MP4 "images" when rendering markdown

--- makeCourse/markdownRenderer/image_processor/imgproc.py
+++ makeCourse/markdownRenderer/image_processor/imgproc.py
@@ -2,6 +2,8 @@ import markdown
 from markdown.treeprocessors import Treeprocessor
 from markdown import Extension
 from markdown.util import etree
+import subprocess
+import shlex
 from copy import copy
 from shutil import copyfile
 from pathlib import Path, PurePath
@@ -14,6 +16,9 @@ class ImageTreeprocessor(Treeprocessor):
         self._item_sourcedir = Path(item_sourcedir)
         self._item_outdir = Path(item_outdir)
         self.imageID = 0
+
+    def pdf2png(self, src, dst):
+        subprocess.run(shlex.split('pdftoppm -png -r 150 -singlefile {} {}'.format(src,dst)))
 
     def run(self, root):
         moved_images = set()
@@ -29,7 +34,11 @@ class ImageTreeprocessor(Treeprocessor):
                     outSrc = Path('images') / (str(self.imageID).zfill(4) + '-' + Path(src).name)
                     outFile = self._item_outdir / outSrc
                     mkdir_p(outFile.parent)
-                    copyfile(str(imageFile),str(outFile))
+                    if imageFile.suffix == '.pdf':
+                        self.pdf2png(str(imageFile),str(outFile.with_suffix('')))
+                        outSrc = outSrc.with_suffix('.png')
+                    else:
+                        copyfile(str(imageFile),str(outFile))
                     image.attrib["src"] = str(outSrc)
                     self.imageID = self.imageID + 1
             moved_images.add(image)
---
+++ makeCourse/themes/default/static/mp4.js
@@ -0,0 +1,27 @@
+// MP4 Video Fallback
+// Based on code by @shshaw: https://codepen.io/shshaw/pen/MOMezY
+var videoAttr = { 'loop': '', 'autoplay':'', 'muted': '', 'playsinline': '' };
+var imgMP4s = Array.prototype.map.call(
+	document.querySelectorAll('img[src*=".mp4"]'),
+	function(img){
+		var src = img.src;
+		img.src = null;
+		img.addEventListener('error', function(e){
+			console.log('MP4 in image not supported. Replacing with video', e); 
+			var video = document.createElement('video');
+			for (var key in videoAttr) { video.setAttribute(key, videoAttr[key]); }
+			for (
+				var imgAttr = img.attributes, 
+				len = imgAttr.length,
+				i = 0; 
+				i < len; 
+				i++
+			) { 
+				video.setAttribute(imgAttr[i].name,  imgAttr[i].value);
+				video.muted = true;
+			}
+			img.parentNode.insertBefore(video, img);
+			img.parentNode.removeChild(img);
+		});
+		img.src = src;
+	});
