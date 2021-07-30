chirun-ncl/chirun

commit 67a2edbcf4fe2524bb0be1f68f6578a0271c7b4a

Improve handling document items with splitlevel>0
Also includes changes to handle div and collapse environments better
from .tex source.

makeCourse/item.py
@@ -128,8 +128,10 @@ class Item(object):
         return Path(self.in_file.stem)
 
     def markdown_content(self, out_format='html'):
-        ext = self.source.suffix
+        if 'html' in self.data:
+            return self.data['html']
 
+        ext = self.source.suffix
         if ext == '.md':
             with open(str(self.course.get_root_dir() / self.source), encoding='utf-8') as f:
                 mdContents = f.read()
@@ -146,8 +148,10 @@ class Item(object):
         return body
 
     def as_html(self):
+        if 'html' in self.data:
+            return self.data['html']
+
         ext = self.source.suffix
-        
         if ext == '.md':
             outPath = (self.course.get_build_dir() / self.out_file).parent
             html = self.markdownRenderer.render(self, outPath)
@@ -171,13 +175,6 @@ class Item(object):
             return {'type': self.type, 'slug': self.slug, 'title': self.title, 'content': [item.content_tree() for item in self.content]}
         else:
             return {'type': self.type, 'slug': self.slug, 'title': self.title, 'source': str(self.source)}
-
-class NoContentMixin:
-    def markdown_content(self,*args,**kwargs):
-        return ''
-
-    def as_html(self):
-        return ''
 
 class Html(Item):
     type = 'html'
@@ -207,10 +204,12 @@ class Part(Item):
     type = 'part'
     title = 'Untitled part'
     template_name = 'part.html'
+    pdf_url = False
 
     def __init__(self, course, data, parent=None):
         super().__init__(course, data, parent)
         self.leading_text = self.data.get('leading_text', '')
+        self.location = self.data.get('location', 'below')
 
     def get_context(self):
         context = super().get_context()
@@ -219,16 +218,6 @@ class Part(Item):
             'chapters': [item.get_context() for item in self.content if not item.is_hidden],
         })
         return context
-
-    def markdown_content(self, *args, **kwargs):
-        if self.data.get('source', None) is not None:
-            return Item.markdown_content(self, *args, **kwargs)
-        return NoContentMixin.markdown_content(self, *args, **kwargs)
-
-    def as_html(self):
-        if self.data.get('source', None) is not None:
-            return Item.as_html(self)
-        return NoContentMixin.as_html(self)
 
 class Document(Item):
     type = 'document'
@@ -243,6 +232,8 @@ class Document(Item):
         super().__init__(course, data, parent)
         self.has_sidebar = self.data.get('sidebar', self.has_sidebar)
         self.has_topbar = self.data.get('topbar', self.has_topbar)
+        self.splitlevel = self.data.get('splitlevel', self.splitlevel)
+        self.has_pdf = self.course.config['build_pdf']
 
     def generate_chapter_subitems(self):
         ext = self.source.suffix
@@ -250,15 +241,39 @@ class Document(Item):
             if not self.generated:
                 self.generated = True
                 plastex_output = self.course.load_latex_content(self)
-                for fn,chapter in plastex_output.items():
-                    if fn != 'index.html':
-                        chapter['html'] = burnInExtras(self, chapter['html'], out_format='html')
+                last_item = dict([(-1, self)])
+                for fn, chapter in plastex_output.items():
+                    chapter['html'] = burnInExtras(self, chapter['html'], out_format='html')
+                    if chapter['html'].isspace():
+                        chapter['html'] = ''
+                    if chapter['level'] < 0:
+                        self.data['html'] = chapter['html']
+                        last_item[-1] = self
+                    elif chapter['level'] < self.splitlevel:
+                        i = -1
+                        while i < chapter['level']:
+                            if i in last_item:
+                                parent = last_item[i]
+                            i = i + 1
+                        item = Part(self.course, chapter, parent)
+                        item.has_sidebar = self.has_sidebar
+                        item.has_topbar = self.has_topbar
+                        item.has_pdf = self.course.config['build_pdf']
+                        item.pdf_url = self.pdf_url
+                        last_item[chapter['level']] = item
+                        parent.content.append(item)
+                    elif chapter['level'] == self.splitlevel:
+                        i = -1
+                        while i < chapter['level']:
+                            if i in last_item:
+                                parent = last_item[i]
+                            i = i + 1
                         item = Html(self.course, chapter, self)
                         item.has_sidebar = self.has_sidebar
                         item.has_topbar = self.has_topbar
                         item.has_pdf = self.course.config['build_pdf']
                         item.pdf_url = self.pdf_url
-                        self.content.append(item)
+                        parent.content.append(item)
         else:
             raise Exception("Error: Unrecognised source type used for LaTeX Document item {}: {}.".format(self.title, self.source))
 
@@ -276,7 +291,7 @@ class Document(Item):
         })
         return context
 
-class Url(NoContentMixin, Item):
+class Url(Item):
     type = 'url'
     title = 'Untitled URL'
     template_name = 'part.html'
@@ -284,6 +299,7 @@ class Url(NoContentMixin, Item):
     def __init__(self, course, data, parent=None):
         super().__init__(course, data, parent)
         self.source = self.data.get('source', '')
+        self.data['html'] = ''
 
     def get_context(self):
         return {
@@ -380,7 +396,6 @@ class Introduction(Item):
     def __init__(self, course, data, parent=None):
         super().__init__(course, data, parent)
         self.leading_text = self.data.get('leading_text', '')
-        self.location = self.data.get('location', 'below')
 
     def __str__(self):
         return 'introduction'

makeCourse/makecourse.sty
@@ -13,13 +13,14 @@
 		\else%
 			\usepackage{a4wide}
 		\fi%
-		\excludecomment{div}
+		\excludecomment{divEnv}
 		\excludecomment{HTML}
 		\newcommand{\numbas}[2][Visit the URL below to try a numbas exam:]{\bigskip\noindent\textbf{Test Yourself} \textrm{#1}\\\expandafter\url{#2}\par}
 		\newcommand{\vimeo}[2][Visit the URL below to view a video:]{\bigskip\noindent\textbf{Video} \textrm{#1}\\\expandafter\url{https://player.vimeo.com/video/#2}\par}
 		\newcommand{\youtube}[2][Visit the URL below to view a video:]{\bigskip\noindent\textbf{Video} \textrm{#1}\\\expandafter\url{https://www.youtube.com/embed/#2}\par}
 		\newcommand{\embed}[1]{\url{#1}}
 		\newcommand{\iframe}[2][]{\url{#2}}
+		\newenvironment{collapseEnv}[3]{\noindent\textbf{#3}\\}{\par}
 		\newcommand{\collapse}[4]{\noindent\textbf{#3}\\{#4}\par}
 		\newcommand{\collapsehint}[1]{\collapse{btn-info}{Give me a hint}{Hint}{#1}}
 		\newcommand{\collapsesolution}[1]{\collapse{btn-primary}{Show Solution}{Solution}{#1}}

makeCourse/plasTeXRenderer/collapse.jinja2s
@@ -8,6 +8,16 @@ name: collapse
 {{ obj.attributes.hintText }}
 </div>
 
+name: collapseEnv
+<p>
+<button class="btn {{ obj.attributes.btnClass }}" data-toggle="collapse" data-target="#{{ obj.id }}">
+{{ obj.attributes.btnText }}
+</button>
+</p>
+<div id="{{ obj.id }}" class="collapse collapse-content">
+{{ obj }}
+</div>
+
 name: collapsehint
 <p>
 <button class="btn btn-info" data-toggle="collapse" data-target="#{{ obj.id }}">

makeCourse/plasTeXRenderer/embed.jinja2s
@@ -22,7 +22,7 @@ src="{{ obj.attributes.content }}" {{ obj.attributes.options.attrs }}></iframe>
 name: cssclass
 <span class="{{obj.attributes.classes}}">{{obj.attributes.content}}</span>
 
-name: div
+name: divEnv
 <div {% if obj.attributes.classes %}class="{{obj.attributes.classes}}"{% endif %}
 {% if obj.attributes.style %}style="{{obj.attributes.style}}"{% endif %}>{{obj}}</div>
 

makeCourse/plastex/__init__.py
@@ -117,7 +117,8 @@ class PlastexRunner:
                         # TODO: an abstraction for applying the following as a series of filters
                         'html': getEmbeddedImages(self, f.read(), item),
                         'title': section.title if isinstance(section.title,str) else section.title.textContent.strip(),
-                        'source': filepath.name
+                        'source': filepath.name,
+                        'level': section.level
                     }
         return plastex_output
 

makeCourse/plastex/overrides/macros.py
@@ -21,7 +21,7 @@ class cssclass(Command):
 class iframe(Command):
     args = '[ options:dict ] content:str'
 
-class div(Environment):
+class divEnv(Environment):
     args = '[ classes:str ] [ style:str ]'
     blockType = True
 
@@ -41,6 +41,10 @@ class kframe(Environment):
         a = self.parse(tex)
         colors = self.ownerDocument.userdata.getPath('packages/color/colors')
         self.style['background-color'] = colors['shadecolor']
+
+class collapseEnv(Environment):
+    args = 'btnClass:str btnText:str parText'
+    blockType = True
 
 class collapse(Command):
     blockType = True

makeCourse/themes/default/static/dark.css
@@ -214,13 +214,21 @@ nav[data-toggle='toc'] .nav-link.active:focus {
 }
 
 /* coursebuilder */
-.exercise {
+.exercise, .exercise.primary {
 	background-color: #375a7f;
 	border-color: #375a7f;
 }
-.interlude {
-	background-color: #00a379;
-	border-color: #00a379;
+.interlude, .exercise.secondary {
+	background-color: #096e54;
+	border-color: #096e54;
+}
+.exercise.tertiary {
+	background-color: #6e0954;
+	border-color: #6e0954;
+}
+.exercise.muted {
+	background-color: #444444;
+	border-color: #444444;
 }
 
 .tcolorbox {

makeCourse/themes/default/static/light.css
@@ -70,11 +70,17 @@ nav[data-toggle='toc'] .nav-link.active:focus {
 }
 
 /* coursebuilder */
-.exercise {
+.exercise, .exercise.primary {
   background-color: #F6F6FF;
 }
-.interlude {
+.interlude, .exercise.secondary {
   background-color: #eeffd3;
+}
+.exercise.tertiary {
+  background-color: #ffeeee;
+}
+.exercise.muted {
+  background-color: #F6F6F6;
 }
 
 .tcolorbox {

commit c11d086c5cc7b9edf607a3de29999d1384ddae53

Empty fallback md/html for items with no source
Fixes a bug with simple part types introduced in 67a2edb.

makeCourse/item.py
@@ -131,19 +131,21 @@ class Item(object):
         if 'html' in self.data:
             return self.data['html']
 
-        ext = self.source.suffix
-        if ext == '.md':
-            with open(str(self.course.get_root_dir() / self.source), encoding='utf-8') as f:
-                mdContents = f.read()
-            if mdContents[:3] == '---':
-                logger.info('    Note: Markdown file {} contains a YAML header. It will be merged in...'.format(self.source))
-                mdContents = re.sub(r'^---.*?---\n', '', mdContents, re.S)
-            body = mdContents
-        elif ext == '.tex':
-            plastex_output = self.course.load_latex_content(self)
-            body = plastex_output['index.html']['html']
-        else:
-            raise Exception("Error: Unrecognised source type for {}: {}.".format(self.title, self.source))
+        body = ''
+        if 'source' in self.data:
+            ext = self.source.suffix
+            if ext == '.md':
+                with open(str(self.course.get_root_dir() / self.source), encoding='utf-8') as f:
+                    mdContents = f.read()
+                if mdContents[:3] == '---':
+                    logger.info('    Note: Markdown file {} contains a YAML header. It will be merged in...'.format(self.source))
+                    mdContents = re.sub(r'^---.*?---\n', '', mdContents, re.S)
+                body = mdContents
+            elif ext == '.tex':
+                plastex_output = self.course.load_latex_content(self)
+                body = plastex_output['index.html']['html']
+            else:
+                raise Exception("Error: Unrecognised source type for {}: {}.".format(self.title, self.source))
 
         return body
 
@@ -151,15 +153,17 @@ class Item(object):
         if 'html' in self.data:
             return self.data['html']
 
-        ext = self.source.suffix
-        if ext == '.md':
-            outPath = (self.course.get_build_dir() / self.out_file).parent
-            html = self.markdownRenderer.render(self, outPath)
-        elif ext == '.tex':
-            plastex_output = self.course.load_latex_content(self)
-            html = plastex_output['index.html']['html']
-        else:
-            raise Exception("Error: Unrecognised source type for {}: {}.".format(self, self.source))
+        html = ''
+        if 'source' in self.data:
+            ext = self.source.suffix
+            if ext == '.md':
+                outPath = (self.course.get_build_dir() / self.out_file).parent
+                html = self.markdownRenderer.render(self, outPath)
+            elif ext == '.tex':
+                plastex_output = self.course.load_latex_content(self)
+                html = plastex_output['index.html']['html']
+            else:
+                raise Exception("Error: Unrecognised source type for {}: {}.".format(self, self.source))
 
         html = burnInExtras(self, html, out_format='html')
         return html