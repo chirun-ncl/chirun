f87ccf51da822e78b250973e5ea283de680904b5
George Stagg <georgestagg@gmail.com>
2021-11-10 08:49:36 +0000

    Bump version to v0.6.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(name="makeCourse",
       description="Produce flexible and accessible course notes, in a variety of formats, using LaTeX or Markdown source",
-      version="0.5.0",
+      version="0.6.0",
       author="E-Learning Team, School of Mathematics, Statistics & Physics, Newcastle University",
       author_email="george.stagg@ncl.ac.uk",
       url="https://mas-coursebuild.ncl.ac.uk",
