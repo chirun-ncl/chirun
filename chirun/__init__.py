70acee520e41e2a4921dd0bcb1d8f58f47b64232
George Stagg <georgestagg@gmail.com>
2022-02-24 15:25:39 +0000

    Fix autoloading hyperref in chirun plastex package

--- chirun/__init__.py
+++ chirun/__init__.py
@@ -1,3 +1,5 @@
+from plasTeX.Packages.embed import *  # noqa: F401, F403
+from plasTeX.Packages.hyperref import *  # noqa: F401, F403
 import re
 import os
 import yaml
--- chirun/plastex/chirun/__init__.py
+++
@@ -1,2 +0,0 @@
-from plasTeX.Packages.embed import *  # noqa: F401, F403
-from plasTeX.Packages.hyperref import *  # noqa: F401, F403
