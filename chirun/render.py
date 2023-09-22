69f76f6090b63f03e64e87eacbc85f6c442d6ec2
Christian Lawson-Perfect <christianperfect@gmail.com>
2023-09-22 15:49:14 +0100

    PDF build: load files from a temporary HTTP server
    see #186
    
    In order to avoid some web security restrictions, the headless browser
    must load pages from an http:// address instead of file://.
    
    This commit changes the `renderer.get_browser` function to become a
    context manager which starts an HTTP server on an OS-assigned port. The
    server terminates when the context ends.
    
    I've also taken the opportunity to abstract out the repeated code
    shared by the base renderer and the slides renderer.

--- chirun/render.py
+++ chirun/render.py
@@ -2,40 +2,89 @@ import asyncio
 from   babel.support import Translations
 import chirun.filter
 from   chirun.markdownRenderer import MarkdownRenderer
+import contextlib
 import datetime
+from   http.server import HTTPServer, SimpleHTTPRequestHandler
 from   jinja2 import Environment, FileSystemLoader, select_autoescape, contextfilter, TemplateNotFound
 import logging
 import nbformat
 from   notedown import MarkdownReader
-from   pyppeteer import launch
+import pyppeteer
 import re
+import socket
+import threading
 from   urllib.parse import urlparse
 from . import mkdir_p
 
 logger = logging.getLogger(__name__)
 
+def find_free_port():
+    """
+        Find and return the number of an open port.
+    """
+    with socket.socket() as s:
+        s.bind(('', 0))
+        return s.getsockname()[1]
 
-async def get_browser():
+def http_server(directory=None, port=8000, end_signal=None):
+
+    class Handler(SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=directory, **kwargs)
+
+    server_address = ('' ,port)
+    httpd = HTTPServer(server_address, Handler)
+    while not end_signal.is_set():
+        httpd.handle_request()
+
+@contextlib.asynccontextmanager
+async def get_browser(directory):
+    """
+        Context manager which opens a headless browser session through pyppeteer, and starts a local HTTP server. 
+        Both are destroyed when the context is destroyed.
+
+        Yields a pair (browser, http_port)
+    """
     browser = None
     attempts = 0
 
-    while browser is None and attempts < 20:
-        try:
-            browser = await launch({
-                'headless': True,
-                'args': ['--no-sandbox', '--disable-setuid-sandbox', '--single-process']
-            })
-        except Exception:
-            logger.info('Warning: Browser failed to start. Trying again...({})'.format(str(attempts)))
-            attempts = attempts + 1
+    try:
+        end_signal = threading.Event()
+        http_port = find_free_port()
+        http_thread = threading.Thread(
+            target=http_server,
+            kwargs = {
+                'directory': directory,
+                'port': http_port,
+                'end_signal': end_signal
+            },
+            daemon = True
+        )
+        http_thread.start()
+        while browser is None and attempts < 20:
+            try:
+                browser = await pyppeteer.launch({
+                    'headless': True,
+                    'args': [
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox',
+                        '--single-process',
+                        '--disable-web-security',
+                    ],
+                })
+            except Exception:
+                logger.info('Warning: Browser failed to start. Trying again...({})'.format(str(attempts)))
+                attempts = attempts + 1
 
-    if not browser:
-        raise Exception('Error: Headless chrome failed to start!')
+        if not browser:
+            raise Exception('Error: Headless chrome failed to start!')
 
-    return browser
+        yield (browser, http_port)
+    finally:
+        await browser.close()
+        end_signal.set()
 
-
-class Renderer(object):
+class BaseRenderer(object):
     def __init__(self, course):
         self.course = course
         self.env = Environment(
@@ -107,36 +156,68 @@ class Renderer(object):
         }
         return self.render_template(template_file, context)
 
+    def pdf_outPath(self, item):
+        return self.course.get_build_dir() / item.named_out_file.with_suffix('.pdf')
+
+    def pdf_kwargs(self, item):
+        return {
+            'path': self.pdf_outPath(item),
+            'format': 'A4', 
+            'printBackground': True
+        }
+
+    def pdf_absHTMLPath(self, item):
+        return item.out_file
+
     async def to_pdf(self, item):
         if item.recently_built():
             return
+
         self.render_item(item)
-        headerHTML = self.to_html(item, item.template_pdfheader)
-        footerHTML = self.to_html(item, item.template_pdffooter)
 
         logger.info("Printing {} to PDF".format(item))
-        absHTMLPath = self.course.get_root_dir().resolve() / self.course.get_build_dir() / item.out_file
-        outPath = self.course.get_build_dir() / item.named_out_file.with_suffix('.pdf')
-        logger.debug('    {src} => {dest}'.format(src=item.title, dest=outPath))
-        browser = await get_browser()
-        page = await browser.newPage()
-        await page.goto('file://{}'.format(absHTMLPath))
-        await page.waitForFunction('window.mathjax_is_loaded == 1', options={'timeout': 10000})
+
+        root_dir = self.course.get_build_dir().resolve()
+        absHTMLPath = self.pdf_absHTMLPath(item)
+        pdf_kwargs = self.pdf_kwargs(item)
+
+        logger.debug('    {src} => {dest}'.format(src = item.title, dest = pdf_kwargs['path']))
+
+        async with get_browser(root_dir) as (browser, port):
+            page = await browser.newPage()
+            page_url = f'http://localhost:{port}/{absHTMLPath}'
+            await page.goto(page_url)
+            await self.wait_for_pdf_ready(page)
+            await page.pdf(pdf_kwargs)
+
+    async def wait_for_pdf_ready(self, page):
+        await page.waitForFunction('window.mathjax_is_loaded == 1', options={'timeout': 100000})
         await page.evaluate(r'''(function() { MathJax.typesetPromise().then(() => window.mathjax_has_run = true)})''')
         await page.waitForFunction('window.mathjax_has_run && document.fonts.ready', options={'timeout': 10000})
-        await page.pdf({'path': outPath, 'format': 'A4', 'displayHeaderFooter': True,
-                        'headerTemplate': headerHTML, 'footerTemplate': footerHTML,
-                        'printBackground': True})
-        await browser.close()
 
+class Renderer(BaseRenderer):
+    def pdf_kwargs(self, item):
+        kwargs = super().pdf_kwargs(item)
 
-class SlidesRenderer(Renderer):
+        headerHTML = self.to_html(item, item.template_pdfheader)
+        footerHTML = self.to_html(item, item.template_pdffooter)
+        kwargs.update({
+            'displayHeaderFooter': True,
+            'headerTemplate': headerHTML,
+            'footerTemplate': footerHTML,
+        })
+
+        return kwargs
+
+class SlidesRenderer(BaseRenderer):
     def __init__(self, course):
         super().__init__(course)
 
     def render_item(self, item):
+        # First build the item in document format.
         super().render_item(item)
 
+        # Then build it as slides.
         outPath = self.course.get_build_dir() / item.out_slides
         template_file = item.template_slides
 
@@ -149,24 +230,16 @@ class SlidesRenderer(Renderer):
         with open(str(outPath), 'w', encoding='utf-8') as f:
             f.write(html)
 
-    async def to_pdf(self, item):
-        if item.recently_built():
-            return
-        self.render_item(item)
-        logger.info("Printing {} as PDF".format(item))
-        absHTMLPath = (self.course.get_root_dir().resolve()
-                       / self.course.get_build_dir()
-                       / item.named_out_file.with_suffix('.slides.html'))
-        outPath = self.course.get_build_dir() / item.named_out_file.with_suffix('.pdf')
-        logger.debug('    {src} => {dest}'.format(src=item.title, dest=outPath))
-        browser = await get_browser()
-        page = await browser.newPage()
-        await page.goto('file://{}?print-pdf'.format(absHTMLPath))
-        await page.setViewport({'width': 1366, 'height': 768})
-#        await page.waitForFunction('window.mathjax_is_loaded == 1', options={'timeout': 10000})
-        await page.pdf({'path': outPath, 'width': 1366, 'height': 768})
-        await browser.close()
+    def pdf_kwargs(self, item):
+        kwargs = super().pdf_kwargs(item)
+        kwargs['landscape'] = True
+        return kwargs
 
+    def pdf_absHTMLPath(self, item):
+        return item.out_slides
+
+    def pdf_outPath(self, item):
+        return self.course.get_build_dir() / item.named_out_file.with_suffix('.pdf')
 
 class NotebookRenderer(object):
     def __init__(self, course):
