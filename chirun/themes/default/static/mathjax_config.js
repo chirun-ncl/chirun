22207b91f36f0c105566e6fbd5d1727882285d7f
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-01-23 13:18:29 +0000

    thinking about how to resolve refs in math mode
    see #219
    
    This adds an overridden handler for the \ref command in MathJax.
    
    plasTeX is supposed to add a <script> tag with the id `plastex-labels`
    to the end of the document, giving a mapping from LaTeX label names to
    their text presentations.
    
    The MathJax ref handler looks up labels in this dictionary and makes
    reference nodes as appropriate.
    
    This works for single-page `chapter` items, but `document` items need
    more thought: plasTeX splits up the HTML into several files, and the
    `plastex-labels` script is only included on the top-level file.
    
    It looks like there's also a problem with text-mode refs in this case,
    anyway: the URLs from plasTeX, which are relative to its output path,
    aren't rewritten by Chirun.

--- chirun/themes/default/static/mathjax_config.js
+++ chirun/themes/default/static/mathjax_config.js
@@ -1,59 +1,101 @@
 window.MathJax = {
-  tex: {
-    macros: {
-      mathsterling: '{\\unicode{xA3}}',
-      pounds: '{\\unicode{xA3}}',
-      euro: '\\unicode{x20AC}',
-      bm: ["\\boldsymbol{ #1 }",1],
-      lefteqn: ["\\rlap{ #1 }\\quad",1],
-      qedhere: "\\tag*{$\\blacksquare$}",
-      pause: ''
-    },
-    inlineMath: [['\\(','\\)']],
-    autoload: {},
-    packages: {'[+]': [
-        'noerrors',
-        'mhchem',
-        'textmacros',
-        'mathtools'
-    ]},
-    textmacros: {
-      packages: {'[+]': ['bbox']}
-    }
-  },
-  startup: {
-    typeset: true,
-    ready: () => {
-      MathJax.startup.defaultReady();
-      MathJax.startup.promise.then(() => {
-        window.mathjax_is_loaded = 1;
-      });
-    }
-  },
-  options: {
-    ignoreHtmlClass: 'tex2jax_ignore',
-    processHtmlClass: 'tex2jax_process',
-    renderActions: {
-      findScript: [10, function (doc) {
-        for (const node of document.querySelectorAll('script[type^="math/tex"]')) {
-          const display = !!node.type.match(/; *mode=display/);
-          const math = new doc.options.MathItem(node.textContent, doc.inputJax[0], display);
-          const text = document.createTextNode('');
-          node.parentNode.replaceChild(text, node);
-          math.start = {node: text, delim: '', n: 0};
-          math.end = {node: text, delim: '', n: 0};
-          doc.math.push(math);
+    tex: {
+        macros: {
+            mathsterling: '{\\unicode{xA3}}',
+            pounds: '{\\unicode{xA3}}',
+            euro: '\\unicode{x20AC}',
+            bm: ["\\boldsymbol{ #1 }",1],
+            lefteqn: ["\\rlap{ #1 }\\quad",1],
+            qedhere: "\\tag*{$\\blacksquare$}",
+            pause: ''
+        },
+        inlineMath: [['\\(','\\)']],
+        autoload: {},
+        packages: {'[+]': [
+            'noerrors',
+            'mhchem',
+            'textmacros',
+            'mathtools',
+            'tagformat',
+            'chirun-eqref'
+        ]},
+        textmacros: {
+            packages: {'[+]': ['bbox']}
+        },
+        tagformat: {
+            url: (id, base) => (id.indexOf('#') >= 0 ? id : base + '#' + encodeURIComponent(id))
         }
-      }, '']
+    },
+    startup: {
+        typeset: true,
+        ready: () => {
+            const Configuration = MathJax._.input.tex.Configuration.Configuration;
+            const CommandMap = MathJax._.input.tex.SymbolMap.CommandMap;
+            const Label = MathJax._.input.tex.Tags.Label;
+            const BaseMethods = MathJax._.input.tex.base.BaseMethods.default;
+
+            const body_refs = {};
+            const plastex_labels = document.getElementById('plastex-labels');
+            if(plastex_labels) {
+                Object.assign(body_refs, JSON.parse(plastex_labels.textContent));
+            }
+            console.log(body_refs);
+            new CommandMap('chirun-eqref', {
+                ref:     ['HandleRef', false],
+                eqref: ['HandleRef', true]
+            }, {
+                HandleRef(parser, name, eqref) {
+                    const i = parser.i;
+                    const label = parser.GetArgument(name);
+                    parser.i = i;
+
+                    const ref = parser.tags.allLabels[label] || parser.tags.labels[label];
+
+                    if (!ref && parser.tags.refUpdate) {
+                        if(body_refs[label]) {
+                            parser.tags.labels[label] = new Label(body_refs[label], parser.tags.formatUrl('#' + encodeURIComponent(label), ''));
+                        }
+                    }
+
+                    BaseMethods.HandleRef(parser, name, eqref);
+                }
+            });
+
+            Configuration.create('chirun-eqref', {
+                handler: {macro: ['chirun-eqref']}
+            });
+
+            MathJax.startup.defaultReady();
+            MathJax.startup.promise.then(() => {
+                window.mathjax_is_loaded = 1;
+            });
+        }
+    },
+    options: {
+        ignoreHtmlClass: 'tex2jax_ignore',
+        processHtmlClass: 'tex2jax_process',
+        renderActions: {
+            findScript: [10, function (doc) {
+                for (const node of document.querySelectorAll('script[type^="math/tex"]')) {
+                    const display = !!node.type.match(/; *mode=display/);
+                    const math = new doc.options.MathItem(node.textContent, doc.inputJax[0], display);
+                    const text = document.createTextNode('');
+                    node.parentNode.replaceChild(text, node);
+                    math.start = {node: text, delim: '', n: 0};
+                    math.end = {node: text, delim: '', n: 0};
+                    doc.math.push(math);
+                }
+            }, '']
+        }
+    },
+    loader: {
+        load: [
+            '[tex]/noerrors',
+            '[tex]/mhchem',
+            '[tex]/textmacros',
+            '[tex]/bbox',
+            '[tex]/mathtools',
+            '[tex]/tagformat'
+        ]
     }
-  },
-  loader: {
-    load: [
-        '[tex]/noerrors',
-        '[tex]/mhchem',
-        '[tex]/textmacros',
-        '[tex]/bbox',
-        '[tex]/mathtools'
-    ]
-  }
 };
