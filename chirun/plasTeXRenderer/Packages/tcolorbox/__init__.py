61e0ddff556c1fb85da9860bedf7b7f0d6d97592
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-01-23 10:49:24 +0000

    support more tcolorbox options and \newtcolorbox
    The \newtcolorbox{name}{options} command can be used to define new
    tcolorbox environments with default options.
    Example:
    
        \newtcolorbox{mybox}{colframe=red, colback=lightblue, title=Example}
    
        \begin{mybox}
            This is an example box.
        \end{mybox}
    
    The following tcolorbox options are supported: colback, colframe,
    coltitle, title, boxrule.

--- chirun/plasTeXRenderer/Packages/tcolorbox/__init__.py
+++ chirun/plasTeXRenderer/Packages/tcolorbox/__init__.py
@@ -1,22 +1,62 @@
 from chirun.plasTeXRenderer import add_package_templates
-from plasTeX import Environment
+import chirun.plasTeXRenderer.Packages.color
+from plasTeX import Command, Environment, CSSStyles
 
-from chirun.plastex.color import latex2htmlcolor
-
+def latex2htmlcolor(*args, **kwargs):
+    return chirun.plasTeXRenderer.Packages.color.latex2htmlcolor(*args, named=self.ownerDocument.userdata.getPath('packages/color/colors'), **kwargs)
 
 def ProcessOptions(options, document):
     add_package_templates(document, package='tcolorbox')
 
-
 class tcolorbox(Environment):
     args = '[ options:dict ]'
     blockType = True
+    boxName = None  # Custom boxes defined with ``\newtcolorbox`` set ``boxName``.
+    boxOptions = {}
 
     def invoke(self, tex):
+        self.title_style = CSSStyles()
+
         res = Environment.invoke(self, tex)
         a = self.attributes
-        if a.get('options'):
-            if a['options'].get('colback'):
-                a['colback'] = latex2htmlcolor(a['options']['colback'],
-                                               named=self.ownerDocument.userdata.getPath('packages/color/colors'))
+        options = {}
+        options.update(self.boxOptions)
+        extra_options = a.get('options')
+        if extra_options:
+            options.update(extra_options)
+
+        colback = options.get('colback')
+        if colback is not None:
+            self.style['background-color'] = colback
+
+        colframe = options.get('colframe')
+        if colframe is not None:
+            self.style['border-color'] = colframe
+
+        boxrule = options.get('boxrule')
+        if boxrule is not None:
+            self.style['border-width'] = boxrule
+
+        title = options.get('title')
+        if title:
+            self.title = title
+
+        coltitle = options.get('coltitle')
+        if coltitle is not None:
+            self.title_style['color'] = coltitle
+
         return res
+
+
+class newtcolorbox(Command):
+    args = 'name:str options:dict'
+
+    def invoke(self, tex):
+        self.parse(tex)
+        attrs = self.attributes
+        name = attrs['name']
+        options = attrs['options']
+
+        # Begin portion
+        newclass = type(name, (tcolorbox,), {'boxOptions': options, 'nodeName': 'tcolorbox', 'boxName': name, })
+        self.ownerDocument.context.addGlobal(name, newclass)
