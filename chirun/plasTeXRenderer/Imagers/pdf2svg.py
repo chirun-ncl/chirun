2c555d4a989e7c0474c84add9daafdbc5bb8abb1
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-04-03 09:56:51 +0100

    the pdf2svg imager only adds in the single-pdf method
    The beamer preamble stuff is now in plasTeX, and the executeConverter
    method was never any different from plasTeX's.

--- chirun/plasTeXRenderer/Imagers/pdf2svg.py
+++ chirun/plasTeXRenderer/Imagers/pdf2svg.py
@@ -1,20 +1,12 @@
-#!/usr/bin/env python
-
-import os
 import subprocess
-import binascii
-from plasTeX.Imagers import VectorImager, Image
+from plasTeX.Imagers import pdf2svg
 from pathlib import Path
 
 
-class PDFSVG(VectorImager):
-    """ Imager that uses pdf2svg """
-    fileExtension = '.svg'
-    verifications = ['pdflatex --help', 'which pdf2svg', 'pdfcrop --help']
-    compiler = 'pdflatex'
-    imagesFilename = 'tmp-images-uc.pdf'
-    croppedImagesFilename = 'tmp-images.pdf'
+class PDFSVG(pdf2svg.PDFSVG):
+    """ Imager that uses pdf2svg. """
 
+    # Overridden to run pdf2svg directly on PDF files, instead of sending them through the LaTeX compiler.
     def getImage(self, node):
         name = getattr(node, 'imageoverride', None)
 
@@ -40,46 +32,6 @@ class PDFSVG(VectorImager):
         img = Image(path, self.ownerDocument.config['images'])
         self.staticimages[name] = img
         return img
-
-    def executeConverter(self, outfile=None):
-        if outfile is None:
-            outfile = self.tmpFile.with_suffix('.pdf').name
-
-        subprocess.call(["pdfcrop", outfile, self.tmpFile.with_suffix('.cropped.pdf').name], stdout=subprocess.DEVNULL)
-
-        images = []
-        for no, line in enumerate(open("images.csv")):
-            filename = 'img%d.svg' % no
-            page, output, scale_str = line.split(",")
-            scale = float(scale_str.strip())
-            images.append((filename, output.rstrip()))
-
-            subprocess.run(['pdf2svg', self.tmpFile.with_suffix('.cropped.pdf').name, filename, str(page)], stdout=subprocess.DEVNULL, check=True)
-
-            if scale != 1:
-                tree = ET.parse(filename)
-                root = tree.getroot()
-
-                for attrib in ["width", "height"]:
-                    m = length_re.match(root.attrib[attrib])
-                    if m is None:
-                        raise ValueError
-                    root.attrib[attrib] = "{:.2f}{}".format(float(m.group(1)) * scale, m.group(2))
-
-                tree.write(filename)
-
-        return images
-
-    def writePreamble(self, document):
-        ret = super().writePreamble(document)
-        self.source.write(r'''
-\ifcsname setbeamerfont\endcsname
-\setbeamertemplate{background canvas}[default]
-\setbeamercolor{background canvas}{bg=}
-\beamertemplatenavigationsymbolsempty
-\fi
-''')
-        return ret
 
 
 Imager = PDFSVG
