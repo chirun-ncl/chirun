3834fcebf658a47ba76261fcb23ce809d645e797
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-04-10 16:17:07 +0100

    still working on unit tests
    this adds some convenience methods to the compilation test, to get
    BeautifulSoup for an HTML output, and to get the package manifest.
    
    Next, need to add a basic markdown document.

--- unittests/__init__.py
+++ unittests/__init__.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import json
 import unittest
 from pathlib import Path
 import shutil
@@ -31,8 +32,24 @@ class ChirunCompilationTest(unittest.TestCase):
     def tearDownClass(cls):
         cls.tmpdir.cleanup()
 
+    def get_soup(self, path):
+        """
+            Get a BeautifulSoup instance for the given HTML file in the build directory.
+        """
+
+        with open(self.build_dir / path) as fp:
+            return BeautifulSoup(fp, 'html.parser')
+
+    def get_manifest(self):
+        with open(self.build_dir / 'MANIFEST.json') as fp:
+            return json.load(fp)
+
 class BasicTest(ChirunCompilationTest):
     source_path = 'basic'
+
+    """
+        Compile a single LaTeX document with almost nothing in it, and check that the general layout is correct.
+    """
 
     def test_compilation_succeeded(self):
         self.assertEqual(self.compilation.returncode, 0)
@@ -42,21 +59,37 @@ class BasicTest(ChirunCompilationTest):
         self.assertTrue(document_dir.exists(), msg=f'{document_dir} exists')
 
     def test_links(self):
-        with open(self.build_dir / "index.html") as fp:
-            intro = BeautifulSoup(fp, 'html.parser')
+        intro = self.get_soup('index.html')
 
         item_links = intro.select('.chirun-structure .item .contents > li > a')
         self.assertEqual(len(item_links), 1, msg="There is only one item")
         item_link = item_links[0]
         self.assertEqual(item_link['href'], 'basic_document/index.html', msg="The link to the item is relative")
 
+    def test_structure(self):
+        manifest = self.get_manifest()
+
+        self.assertEqual(len(manifest['structure']), 2, msg="There are two items in the structure.")
+        self.assertEqual([x['type'] for x in manifest['structure']], ['introduction', 'chapter'], msg="There is an introduction and a chapter.")
+
 class BasicStandaloneTest(ChirunCompilationTest):
+    """
+        Compile a single LaTeX document in standalone mode.
+    """
+
     source_path = 'basic'
     compile_args = ['-f','test.tex']
 
     def test_standalone_compilation(self):
         self.assertEqual(self.compilation.returncode, 0)
 
+    def test_index_exists(self):
+        self.assertTrue((self.build_dir / 'index.html').exists())
+
+    def test_structure(self):
+        manifest = self.get_manifest()
+
+        self.assertEqual(len(manifest['structure']), 1, msg="There is only one item in the structure.")
 
 if __name__ == '__main__':
     unittest.main()
