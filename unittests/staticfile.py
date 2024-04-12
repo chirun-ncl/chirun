8243f79599ae9e46f90452866c8e86188303d76b
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-04-12 11:39:57 +0100

    add a test for #54

--- unittests/staticfile.py
+++ unittests/staticfile.py
@@ -18,3 +18,18 @@ class StaticFileTest(ChirunCompilationTest):
         links = soup.select('.item-content a')
         self.assertEqual(links[0]['href'], '../static/text.txt')
         self.assertEqual(links[1]['href'], '../static/test.tex')
+
+    def test_manifest(self):
+        manifest = self.get_manifest()
+
+        self.assertEqual(manifest['structure'][2]['content'][0]['url'], 'static/text.txt')
+
+    def test_url_item(self):
+        """ Test that the index page for a p art correctly links to a static file.
+
+            Tests https://github.com/chirun-ncl/chirun/issues/54
+        """
+
+        soup = self.get_soup('a_part/index.html')
+
+        self.assertEqual(soup.select_one('.chirun-structure .contents a')['href'], '../static/text.txt')
