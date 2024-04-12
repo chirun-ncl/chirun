23b2a1dfa8e5d12fe43ea8d543cece106d2ac832
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-04-12 12:44:15 +0100

    add a test for #60

--- unittests/maths.py
+++ unittests/maths.py
@@ -2,7 +2,6 @@ from . import ChirunCompilationTest
 
 class MathsTest(ChirunCompilationTest):
     source_path = 'maths'
-    compile_args = ['-f', 'test.tex']
 
     def test_adjacent_inline_maths(self):
         """ Check that two passages of inline math delimited by dollar signs are rendered OK.
@@ -10,6 +9,18 @@ class MathsTest(ChirunCompilationTest):
             Tests https://github.com/chirun-ncl/chirun/issues/50
         """
 
-        soup = self.get_soup('index.html')
+        soup = self.get_soup('latex_maths/index.html')
 
         self.assertIn(r'\(x\)\(y\)', soup.select('.item-content p')[0].text)
+
+    def test_markdown_display_without_newline(self):
+        """ Test that backslashes in display mode maths on the same line as other text work without escaping.
+
+            Tests https://github.com/chirun-ncl/chirun/issues/60
+        """
+
+        soup = self.get_soup('markdown_maths/index.html')
+
+        display_maths = soup.select_one('.item-content p script[type="math/tex; mode=display"]')
+        self.assertEqual(display_maths.text.strip(), r'\frac{1}{2}')
+        print(soup.select('.item-content p'))
---
+++ unittests/maths/test.md
@@ -0,0 +1,9 @@
+# Markdown maths
+
+Inline maths: $x$ \(y\)
+
+Display maths:
+
+\[ z \]
+
+Display maths without a linebreak: \[ \frac{1}{2} \]
