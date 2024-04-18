8ef67f10169f91db38c4868b084b898a842d043f
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-04-18 15:24:35 +0100

    test that the link to a PDF of a standalone doc is correct
    tests #222

--- unittests/pdf.py
+++ unittests/pdf.py
@@ -11,3 +11,20 @@ class PDFTest(ChirunCompilationTest):
 
         self.assertTrue((self.build_dir / 'latex_document_1' / 'latex_document_1.pdf').exists())
         self.assertFalse((self.build_dir / 'latex_document_2' / 'latex_document_2.pdf').exists())
+
+class StandalonePDFLinkTest(ChirunCompilationTest):
+    source_path = 'pdf'
+    compile_args = ['-f', 'test.tex']
+
+    def test_pdf_link(self):
+        """ Check that the link to the PDF is correct.
+
+            Tests https://github.com/chirun-ncl/chirun/issues/222
+        """
+
+        soup = self.get_soup('index.html')
+
+        self.assertTrue((self.build_dir / 'document.pdf').exists())
+
+        pdf_link = soup.select_one('a[rel="alternate"][type="application/pdf"]')
+        self.assertEqual(pdf_link['href'], 'document.pdf')
