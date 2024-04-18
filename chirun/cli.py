31a7972a75b2cf05a509a9a59d237aaea80f02ac
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-04-18 14:20:59 +0100

    course.build_dir is absolute
    This fixes a problem where make_relative_url wouldn't recognise the
    paths produced by plasTeX

--- chirun/cli.py
+++ chirun/cli.py
@@ -43,7 +43,7 @@ class Chirun:
         self.force_relative_build = False
 
         self.root_dir = self.get_root_dir()
-        self.build_dir = Path(args.build_path) if args.build_path is not None else self.root_dir / 'build'
+        self.build_dir = (Path(args.build_path) if args.build_path is not None else (self.root_dir / 'build')).resolve()
 
         self.hidden_paths = []  # A list of directories containing hidden items, filled in by process.FindHiddenItemsProcess.
 
