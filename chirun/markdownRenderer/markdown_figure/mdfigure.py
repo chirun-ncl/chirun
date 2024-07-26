922cc01120d8df50ecf0e0be793446b628dd18a0
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-07-26 14:43:55 +0100

    rewrite the mdfigure extension
    fixes #233
    
    The mdfigure extension was trying to wrap images in <figure> elements,
    but it was doing so while enumerating through image elements, which
    meant that it was skipping every other image, and the images would end
    up out of order.
    
    This commit rewrites the extension from scratch to be much simpler: when
    a <p> tag has only image children and no text content, it's changed to a
    <figure> tag.
    
    It looks like the old code was obtained from
    https://github.com/janw/markdown-figure, uncredited.

--- chirun/markdownRenderer/markdown_figure/mdfigure.py
+++ chirun/markdownRenderer/markdown_figure/mdfigure.py
@@ -1,89 +1,26 @@
 """
-Extension for markbox to make all images lightbox
-
-copyright @2015 Alicia Schep <aschep@gmail.com>
-
+Replace `<p>` tags whose only children are images with `<figure>` tags.
 """
-import markdown
+
 from markdown.treeprocessors import Treeprocessor
 from markdown import Extension
-from markdown.util import etree
-from copy import copy
 
+def safe_text(txt):
+    return txt if txt is not None else ''
 
 class FigureTreeprocessor(Treeprocessor):
-    """ Figure Treeprocessor """
-
-    def __init__(self, md, figure_classes=None, img_classes=None):
-        Treeprocessor.__init__(self, md)
-        self._figure_classes = figure_classes
-        self._img_classes = img_classes
-
     def run(self, root):
-        moved_images = set()
-        parent_map = {c: p for p in root.iter() for c in p}
-        images = root.iter("img")
-        for count, image in enumerate(images):
-            if image in moved_images:
-                continue
-            alttext = image.attrib["alt"]
-            image.set("alt", alttext)
-            parent = parent_map[image]
-            idx = list(parent).index(image)
+        img_tags = ('img', 'svg', 'object')
 
-            figure = etree.Element('figure')
+        for p in root.iter('p'):
+            text_content = safe_text(p.text) + ''.join(safe_text(c.tail) for c in p)
 
-            if self._figure_classes is not None:
-                figure.set("class", self._figure_classes)
-            else:
-                figure.set("class", "figure" + str(count + 1))
-
-            if self._img_classes is not None:
-                image.set("class", self._img_classes)
-
-            figure.set("data-title", alttext)
-            figure.tail = copy(image.tail)
-            parent.remove(image)
-            if parent.tag == 'p':
-                grandparent = parent_map[parent]
-                pidx = list(grandparent).index(parent)
-                afters = list(parent)[idx + 1:]
-                grandparent.insert(pidx + 1, figure)
-                if len(afters) or figure.tail:
-                    afters_parent = etree.Element('p', parent.attrib)
-                    afters_parent.text = figure.tail
-                    figure.tail = None
-                    for a in afters:
-                        parent.remove(a)
-                        afters_parent.append(a)
-                        parent_map[a] = afters_parent
-                        parent_map[afters_parent] = grandparent
-                    grandparent.insert(pidx + 2, afters_parent)
-                if not (len(list(parent)) or parent.text):
-                    grandparent.remove(parent)
-            else:
-                parent.insert(idx, figure)
-            image.tail = markdown.util.AtomicString("")
-            figure.append(image)
-            moved_images.add(image)
-
+            if all(c.tag in img_tags for c in p) and text_content.strip()=='':
+                p.tag = 'figure'
+        return
 
 class FigureExtension(Extension):
-    """
-    LightboxImagesExtension
-    Extension class for markdown
-    """
-
-    def __init__(self, **kwargs):
-        self.config = {
-            'figure_classes': [None, "Class attributes assigned to the <figure /> tag"],
-            'img_classes': [None, "Class attributes assigned to the <img /> tag"],
-        }
-        super(FigureExtension, self).__init__(**kwargs)
-
     def extendMarkdown(self, md, md_globals):
-        figures = FigureTreeprocessor(md,
-                                      self.getConfig('figure_classes'),
-                                      self.getConfig('img_classes'))
+        figures = FigureTreeprocessor(md)
         md.treeprocessors.add("figure", figures, "_end")
         md.registerExtension(self)
