537b27dff472c2a86e365d932ae3f01536309c6c
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-09-25 15:37:20 +0100

    deal with `\xymatrix`'s setup options
    `\xymatrix` takes optional setup commands before the `{`
    character that opens its argument.
    
    I think the setup commands all start with an @, but since the imager is
    going to deal with them anyway, this just scans until the first `{`
    character.

--- chirun/plasTeXRenderer/Packages/xypic.py
+++ chirun/plasTeXRenderer/Packages/xypic.py
@@ -1,9 +1,18 @@
 from plasTeX.Base import Command
+from plasTeX.Tokenizer import Token
 
 class xymatrix(Command):
     args = 'picture'
 
     altText = None
+
+    def parse(self, tex):
+        self.argSource = ''
+        while True:
+            output, source = tex.readArgumentAndSource(parentNode=self)
+            self.argSource += source
+            if output is None or any(tok.catcode == Token.CC_BGROUP for tok in output):
+                break
 
     def invoke(self, tex):
         gfx = super().invoke(tex)
